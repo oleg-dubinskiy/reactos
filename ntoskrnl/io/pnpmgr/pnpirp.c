472523f24be09df61461731258dcb3397eeefc6b
Vadim Galyant <vgal@rambler.ru>
2019-06-03 11:45:59 +0900

    [NTOS:PNP] Add IopIncrementDeviceObjectHandleCount().

--- ntoskrnl/io/pnpmgr/pnpirp.c
+++ ntoskrnl/io/pnpmgr/pnpirp.c
@@ -1110,6 +1110,15 @@ IopFindMountableDevice(
     return MountableDevice;
 }
 
+ULONG
+NTAPI
+IopIncrementDeviceObjectHandleCount(
+    _Inout_ PDEVICE_OBJECT FileDeviceObject)
+{
+    return IopInterlockedIncrementUlong(LockQueueIoDatabaseLock,
+                                        &FileDeviceObject->ReferenceCount);
+}
+
 PDEVICE_OBJECT
 NTAPI
 IopLockMountedDeviceForRemove(
