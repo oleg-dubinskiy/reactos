56f9ba32806ecc75af1d0859176adb6502727549
Vadim Galyant <vgal@rambler.ru>
2022-04-12 20:32:28 +0400

    [LIB:ARBITER] Add ArbpGetRegistryValue() and implement ArbBuildAssignmentOrdering().

--- sdk/lib/drivers/arbiter/arbiter.c
+++ sdk/lib/drivers/arbiter/arbiter.c
@@ -203,7 +203,7 @@ ArbAddOrdering(
     _In_ ULONGLONG MaximumAddress)
 {
     PARBITER_ORDERING NewOrderings;
-    UINT32 NewCountSize;
+    ULONG NewCountSize;
 
     PAGED_CODE();
 
@@ -262,8 +262,8 @@ ArbPruneOrdering(
     PARBITER_ORDERING Orderings;
     PARBITER_ORDERING NewOrderings;
     PARBITER_ORDERING TmpOrderings;
-    UINT32 TmpOrderingsSize;
-    UINT32 ix;
+    ULONG TmpOrderingsSize;
+    ULONG ix;
     USHORT Count;
 
     PAGED_CODE();
@@ -406,6 +406,61 @@ ArbFreeOrderingList(
     OrderList->Orderings = NULL;
 }
 
+static
+NTSTATUS
+ArbpGetRegistryValue(
+    _In_ HANDLE KeyHandle,
+    _In_ PCWSTR NameString,
+    _Out_ PKEY_VALUE_FULL_INFORMATION * OutValueInfo)
+{
+    PKEY_VALUE_FULL_INFORMATION ValueInfo;
+    UNICODE_STRING ValueName;
+    ULONG ResultLength;
+    NTSTATUS Status;
+
+    PAGED_CODE();
+
+    DPRINT("ArbpGetRegistryValue: NameString '%S'\n", NameString);
+    RtlInitUnicodeString(&ValueName, NameString);
+
+    Status = ZwQueryValueKey(KeyHandle,
+                             &ValueName,
+                             KeyFullInformation | KeyNodeInformation,
+                             NULL,
+                             0,
+                             &ResultLength);
+
+    if (Status != STATUS_BUFFER_OVERFLOW && Status != STATUS_BUFFER_TOO_SMALL)
+    {
+        DPRINT1("ArbpGetRegistryValue: Status %X\n", Status);
+        return Status;
+    }
+
+    ValueInfo = ExAllocatePoolWithTag(PagedPool, ResultLength, TAG_ARBITER);
+    if (!ValueInfo)
+    {
+        DPRINT1("ArbpGetRegistryValue: STATUS_INSUFFICIENT_RESOURCES\n");
+        return STATUS_INSUFFICIENT_RESOURCES;
+    }
+
+    Status = ZwQueryValueKey(KeyHandle,
+                             &ValueName,
+                             KeyFullInformation|KeyNodeInformation,
+                             ValueInfo,
+                             ResultLength,
+                             &ResultLength);
+    if (NT_SUCCESS(Status))
+    {
+        *OutValueInfo = ValueInfo;
+        return STATUS_SUCCESS;
+    }
+
+    DPRINT1("ArbpGetRegistryValue: Status %X\n", Status);
+
+    ExFreePoolWithTag(ValueInfo, TAG_ARBITER);
+    return Status;
+}
+
 NTSTATUS
 NTAPI
 ArbBuildAssignmentOrdering(
@@ -414,10 +469,244 @@ ArbBuildAssignmentOrdering(
     _In_ PCWSTR ReservedOrderName,
     _In_ PARB_TRANSLATE_ORDERING TranslateOrderingFunction)
 {
+    UNICODE_STRING ReservedResourcesName = RTL_CONSTANT_STRING(L"ReservedResources");
+    UNICODE_STRING AllocationOrderName = RTL_CONSTANT_STRING(L"AllocationOrder");
+    UNICODE_STRING ArbitersKeyName = RTL_CONSTANT_STRING(IO_REG_KEY_ARBITERS);
+    OBJECT_ATTRIBUTES ObjectAttributes;
+    PKEY_VALUE_FULL_INFORMATION ReservedValueInfo = NULL;
+    PKEY_VALUE_FULL_INFORMATION ValueInfo = NULL;
+    PIO_RESOURCE_REQUIREMENTS_LIST IoResources;
+    PIO_RESOURCE_DESCRIPTOR IoDescriptor;
+    IO_RESOURCE_DESCRIPTOR TranslatedIoDesc;
+    HANDLE ArbitersKeyHandle = NULL;
+    HANDLE OrderingKeyHandle = NULL;
+    PCWSTR CurrentOrderName;
+    PCWSTR ValueName;
+    ULONG ix;
+    NTSTATUS Status;
+  #if DBG
+    PARBITER_ORDERING Orderings;
+  #endif
+
     PAGED_CODE();
 
-    UNIMPLEMENTED;
+    DPRINT("[%p] OrderName '%S', ReservedOrderName '%S'\n", ArbInstance, OrderName, ReservedOrderName);
+
+    KeWaitForSingleObject(ArbInstance->MutexEvent,
+                          Executive,
+                          KernelMode,
+                          FALSE,
+                          NULL);
+
+    ArbFreeOrderingList(&ArbInstance->OrderingList);
+    ArbFreeOrderingList(&ArbInstance->ReservedList);
+
+    Status = ArbInitializeOrderingList(&ArbInstance->OrderingList);
+    if (!NT_SUCCESS(Status))
+        goto ErrorExit;
+
+    Status = ArbInitializeOrderingList(&ArbInstance->ReservedList);
+    if (!NT_SUCCESS(Status))
+        goto ErrorExit;
+
+    InitializeObjectAttributes(&ObjectAttributes,
+                               &ArbitersKeyName,
+                               OBJ_KERNEL_HANDLE | OBJ_CASE_INSENSITIVE,
+                               NULL,
+                               NULL);
+
+    Status = ZwOpenKey(&ArbitersKeyHandle, KEY_READ, &ObjectAttributes);
+    if (!NT_SUCCESS(Status))
+        goto ErrorExit;
+
+    /* First loop for AllocationOrder, next for ReservedResources */
+    for (ix = 0; ix <= 1; ix++)
+    {
+        ValueInfo = NULL;
+
+        if (ix == 0)
+        {
+            CurrentOrderName = OrderName;
+
+            InitializeObjectAttributes(&ObjectAttributes,
+                                       &AllocationOrderName,
+                                       OBJ_KERNEL_HANDLE | OBJ_CASE_INSENSITIVE,
+                                       ArbitersKeyHandle,
+                                       NULL);
+
+            Status = ZwOpenKey(&OrderingKeyHandle, KEY_READ, &ObjectAttributes);
+        }
+        else
+        {
+            CurrentOrderName = ReservedOrderName;
+
+            InitializeObjectAttributes(&ObjectAttributes,
+                                       &ReservedResourcesName,
+                                       OBJ_KERNEL_HANDLE | OBJ_CASE_INSENSITIVE,
+                                       ArbitersKeyHandle,
+                                       NULL);
+
+            Status = ZwCreateKey(&OrderingKeyHandle,
+                                 KEY_READ,
+                                 &ObjectAttributes,
+                                 0,
+                                 NULL,
+                                 REG_OPTION_NON_VOLATILE,
+                                 NULL);
+        }
+
+        if (!NT_SUCCESS(Status))
+            goto ErrorExit;
+
+        Status = ArbpGetRegistryValue(OrderingKeyHandle, CurrentOrderName, &ValueInfo);
+        if (!NT_SUCCESS(Status) || !ValueInfo)
+            goto ErrorExit;
+
+        if (ix == 1 && ValueInfo->Type == REG_SZ)
+        {
+            /* ReservedResources case */
+
+            ValueName = (PCWSTR)((ULONG_PTR)ValueInfo + ValueInfo->DataOffset);
+            DPRINT("ArbBuildAssignmentOrdering: ValueName '%S'\n", ValueName);
+
+            if (ValueName[(ValueInfo->DataLength / sizeof(WCHAR)) - 1])
+                goto ErrorExit;
+
+            Status = ArbpGetRegistryValue(OrderingKeyHandle, ValueName, &ReservedValueInfo);
+            if (!NT_SUCCESS(Status))
+                goto ErrorExit;
+
+            ExFreePoolWithTag(ValueInfo, TAG_ARBITER);
+            ValueInfo = ReservedValueInfo;
+        }
+
+        ZwClose(OrderingKeyHandle);
+
+        if (ValueInfo->Type != REG_RESOURCE_REQUIREMENTS_LIST)
+        {
+            DPRINT1("ArbBuildAssignmentOrdering: STATUS_INVALID_PARAMETER\n");
+            Status = STATUS_INVALID_PARAMETER;
+            goto ErrorExit;
+        }
+
+        IoResources = (PIO_RESOURCE_REQUIREMENTS_LIST)((ULONG_PTR)ValueInfo + ValueInfo->DataOffset);
+        ASSERT(IoResources->AlternativeLists == 1);
+
+        for (IoDescriptor = &IoResources->List[0].Descriptors[0];
+             IoDescriptor < (&IoResources->List[0].Descriptors[0] + IoResources->List[0].Count);
+             IoDescriptor++)
+        {
+            ULONGLONG MinimumAddress;
+            ULONGLONG MaximumAddress;
+            ULONG Dummy1;
+            ULONG Dummy2;
+
+            if (TranslateOrderingFunction)
+            {
+                Status = TranslateOrderingFunction(&TranslatedIoDesc, IoDescriptor);
+                if (!NT_SUCCESS(Status))
+                    goto ErrorExit;
+            }
+            else
+            {
+                RtlCopyMemory(&TranslatedIoDesc, IoDescriptor, sizeof(TranslatedIoDesc));
+            }
+
+            if (TranslatedIoDesc.Type != ArbInstance->ResourceType)
+                continue;
+
+            Status = ArbInstance->UnpackRequirement(&TranslatedIoDesc,
+                                                    &MinimumAddress,
+                                                    &MaximumAddress,
+                                                    &Dummy1,
+                                                    &Dummy2);
+            if (!NT_SUCCESS(Status))
+                goto ErrorExit;
+
+            if (ix == 0)
+            {
+                /* AllocationOrder */
+                Status = ArbAddOrdering(&ArbInstance->OrderingList, MinimumAddress, MaximumAddress);
+                if (!NT_SUCCESS(Status))
+                    goto ErrorExit;
+            }
+            else
+            {
+                /* ReservedResources */
+                Status = ArbAddOrdering(&ArbInstance->ReservedList, MinimumAddress, MaximumAddress);
+                if (!NT_SUCCESS(Status))
+                    goto ErrorExit;
+
+                /* Prune AllocationOrder */
+                Status = ArbPruneOrdering(&ArbInstance->OrderingList, MinimumAddress, MaximumAddress);
+                if (!NT_SUCCESS(Status))
+                    goto ErrorExit;
+            }
+        }
+    }
+
+    ZwClose(ArbitersKeyHandle);
+
+#if DBG
+
+    Orderings = ArbInstance->OrderingList.Orderings;
+
+    for (Orderings = &ArbInstance->OrderingList.Orderings[0];
+         Orderings < &ArbInstance->OrderingList.Orderings[ArbInstance->OrderingList.Count];
+         Orderings++)
+    {
+        DPRINT("ArbBuildAssignmentOrdering: OrderingList(%I64X-%I64X)\n",
+               Orderings->Start, Orderings->End);
+    }
+
+    Orderings = ArbInstance->ReservedList.Orderings;
+
+    for (Orderings = &ArbInstance->ReservedList.Orderings[0];
+         Orderings < &ArbInstance->ReservedList.Orderings[ArbInstance->ReservedList.Count];
+         Orderings++)
+    {
+        DPRINT("ArbBuildAssignmentOrdering: ReservedList(%I64X-%I64X)\n",
+               Orderings->Start, Orderings->End);
+    }
+
+#endif
+
+    KeSetEvent(ArbInstance->MutexEvent, IO_NO_INCREMENT, FALSE);
     return STATUS_SUCCESS;
+
+ErrorExit:
+
+    DPRINT1("ArbBuildAssignmentOrdering: ErrorExit. Status %X\n", Status);
+
+    if (ArbitersKeyHandle)
+        ZwClose(ArbitersKeyHandle);
+
+    if (OrderingKeyHandle)
+        ZwClose(OrderingKeyHandle);
+
+    if (ValueInfo)
+        ExFreePoolWithTag(ValueInfo, TAG_ARBITER);
+
+    if (ReservedValueInfo)
+        ExFreePoolWithTag(ReservedValueInfo, TAG_ARBITER);
+
+    if (ArbInstance->OrderingList.Orderings)
+    {
+        ExFreePoolWithTag(ArbInstance->OrderingList.Orderings, TAG_ARB_ORDERING);
+        ArbInstance->OrderingList.Count = 0;
+        ArbInstance->OrderingList.Maximum = 0;
+    }
+
+    if (ArbInstance->ReservedList.Orderings)
+    {
+        ExFreePoolWithTag(ArbInstance->ReservedList.Orderings, TAG_ARB_ORDERING);
+        ArbInstance->ReservedList.Count = 0;
+        ArbInstance->ReservedList.Maximum = 0;
+    }
+
+    KeSetEvent(ArbInstance->MutexEvent, IO_NO_INCREMENT, FALSE);
+
+    return Status;
 }
 
 NTSTATUS
