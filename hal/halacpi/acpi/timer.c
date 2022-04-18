a6c5e43eca61bc2c8c447c351cbd17a0c668dc15
Vadim Galyant <vgal@rambler.ru>
2022-04-18 12:18:04 +0400

    [HALACPI] Add HalAcpiDispatchTable.

---
+++ hal/halacpi/acpi/timer.c
@@ -0,0 +1,173 @@
+
+/* INCLUDES ******************************************************************/
+
+#include <hal.h>
+#include "timer.h"
+
+//#define NDEBUG
+#include <debug.h>
+
+/* GLOBALS *******************************************************************/
+
+ACPI_PM_DISPATCH_TABLE HalAcpiDispatchTable =
+{
+    0x48414C20, // 'HAL '
+    2,
+    HaliAcpiTimerInit,
+    NULL,
+    HaliAcpiMachineStateInit,
+    HaliAcpiQueryFlags,
+    HalpAcpiPicStateIntact,
+    HalpRestoreInterruptControllerState,
+    HaliPciInterfaceReadConfig,
+    HaliPciInterfaceWriteConfig,
+    HaliSetVectorState,
+    HalpGetApicVersion,
+    HaliSetMaxLegacyPciBusNumber,
+    HaliIsVectorValid 
+};
+
+HALP_TIMER_INFO TimerInfo;
+BOOLEAN HalpBrokenAcpiTimer = FALSE;
+
+extern FADT HalpFixedAcpiDescTable;
+
+/* PRIVATE FUNCTIONS *********************************************************/
+
+VOID
+NTAPI
+HalaAcpiTimerInit(_In_ PULONG TimerPort,
+                  _In_ BOOLEAN IsTimerValExt)
+{
+    DPRINT("HalaAcpiTimerInit: Port %X, IsValExt %X\n", TimerPort, IsTimerValExt);
+
+    RtlZeroMemory(&TimerInfo, sizeof(TimerInfo));
+
+    TimerInfo.TimerPort = TimerPort;
+
+    if (IsTimerValExt)
+        TimerInfo.ValueExt = 0x80000000; // 32-bit
+    else
+        TimerInfo.ValueExt = 0x00800000; // 24-bit
+
+    if (!HalpBrokenAcpiTimer)
+    {
+        DPRINT("HalpBrokenAcpiTimer - FALSE\n");
+        return;
+    }
+
+    DPRINT1("HalaAcpiTimerInit: FIXME HalpQueryBrokenPiix4()\n");
+    ASSERT(0); // HalpDbgBreakPointEx();
+
+}
+
+VOID
+NTAPI
+HaliAcpiTimerInit(_In_ PULONG TimerPort,
+                  _In_ BOOLEAN TimerValExt)
+{
+    PAGED_CODE();
+
+    DPRINT("HaliAcpiTimerInit: Port %X, ValExt %X\n", TimerPort, TimerValExt);
+    DPRINT("HalpFixedAcpiDescTable.flags - %08X\n", HalpFixedAcpiDescTable.flags);
+
+    /* Is this in the init phase? */
+    if (!TimerPort)
+    {
+        /* Get the data from the FADT */
+
+        /* System port address of the Power Management Timer Control Register Block */
+        TimerPort = (PULONG)HalpFixedAcpiDescTable.pm_tmr_blk_io_port;
+
+        /* A zero indicates TMR_VAL is implemented as a 24-bit value.
+           A one indicates TMR_VAL is implemented as a 32-bit value.
+        */
+        TimerValExt = ((HalpFixedAcpiDescTable.flags & ACPI_TMR_VAL_EXT) != 0);
+        DPRINT1("TimerPort %X, TimerValExt %X\n", TimerPort, TimerValExt);
+    }
+
+    /* FIXME: Now proceed to the timer initialization */
+    HalaAcpiTimerInit(TimerPort, TimerValExt);
+}
+
+
+/* PM DISPATCH FUNCTIONS *****************************************************/
+
+VOID
+NTAPI
+HaliAcpiMachineStateInit(_In_ ULONG Par1,
+                         _In_ PVOID Par2,
+                         _Out_ PVOID OutPar3)
+{
+    UNIMPLEMENTED;
+    ASSERT(0);// HalpDbgBreakPointEx();
+}
+
+ULONG
+NTAPI
+HaliAcpiQueryFlags(VOID)
+{
+    UNIMPLEMENTED;
+    ASSERT(0);// HalpDbgBreakPointEx();
+    return 0;
+}
+
+UCHAR
+NTAPI
+HalpAcpiPicStateIntact(VOID)
+{
+    UNIMPLEMENTED;
+    ASSERT(0);// HalpDbgBreakPointEx();
+    return 0;
+}
+
+VOID
+NTAPI
+HalpRestoreInterruptControllerState(VOID)
+{
+    UNIMPLEMENTED;
+    ASSERT(0);// HalpDbgBreakPointEx();
+}
+
+VOID
+NTAPI
+HaliSetVectorState(_In_ ULONG Par1,
+                   _In_ ULONG Par2)
+{
+    UNIMPLEMENTED;
+    ASSERT(0);// HalpDbgBreakPointEx();
+}
+
+ULONG
+NTAPI
+HalpGetApicVersion(_In_ ULONG Par1)
+{
+    UNIMPLEMENTED;
+    ASSERT(0);// HalpDbgBreakPointEx();
+    return 0;
+}
+
+VOID
+NTAPI
+HaliSetMaxLegacyPciBusNumber(_In_ ULONG MaxLegacyPciBusNumber)
+{
+    UNIMPLEMENTED;
+    ASSERT(0);// HalpDbgBreakPointEx();
+}
+
+BOOLEAN
+NTAPI
+HaliIsVectorValid(_In_ ULONG Vector)
+{
+    DPRINT("HaliIsVectorValid: Vector %X\n", Vector);
+
+    if (Vector >= 0x10)
+    {
+        DPRINT1("HaliIsVectorValid: Vector %X\n", Vector);
+        //ASSERT(Vector < 0x10);
+    }
+
+    return (Vector < 0x10);
+}
+
+/* EOF */
---
+++ hal/halacpi/acpi/timer.h
@@ -0,0 +1,161 @@
+
+#pragma once
+
+#include <pshpack4.h>
+typedef struct _HALP_TIMER_INFO
+{
+    PULONG TimerPort;
+    LARGE_INTEGER AcpiTimeValue;
+    ULONG TimerCarry;
+    ULONG ValueExt;
+    LARGE_INTEGER PerformanceCounter;
+    ULONGLONG Unknown1;
+    ULONG Unknown2;
+} HALP_TIMER_INFO, *PHALP_TIMER_INFO;
+#include <poppack.h>
+
+typedef VOID
+(NTAPI * PHAL_ACPI_TIMER_INIT)(
+    _In_ PULONG TimerPort,
+    _In_ BOOLEAN TimerValExt
+);
+
+typedef VOID
+(NTAPI * PHAL_ACPI_MACHINE_STATE_INIT)(
+    _In_ ULONG Par1,
+    _In_ PVOID Par2,
+    _Out_ PVOID OutPar3
+);
+
+typedef ULONG
+(NTAPI * PHAL_ACPI_QUERY_FLAGS)(
+    VOID
+);
+
+typedef UCHAR
+(NTAPI * PHAL_ACPI_PIC_STATE_INTACT)(
+    VOID
+);
+
+typedef VOID
+(NTAPI * PHAL_RESTORE_INT_CONTROLLER_STATE)(
+    VOID
+);
+
+typedef ULONG
+(NTAPI * PHAL_PCI_INTERFACE_READ_CONFIG)(
+    _In_ PVOID Context,
+    _In_ ULONG BusNumber,
+    _In_ ULONG SlotNumber,
+    _In_ PVOID Buffer,
+    _In_ ULONG Offset,
+    _In_ ULONG Length
+);
+
+typedef ULONG
+(NTAPI * PHAL_PCI_INTERFACE_WRITE_CONFIG)(
+    _In_ PVOID Context,
+    _In_ ULONG BusNumber,
+    _In_ ULONG SlotNumber,
+    _In_ PVOID Buffer,
+    _In_ ULONG Offset,
+    _In_ ULONG Length
+);
+
+typedef VOID
+(NTAPI * PHAL_SET_VECTOR_STATE)(
+    _In_ ULONG Par1,
+    _In_ ULONG Par2
+);
+
+typedef VOID
+(NTAPI * PHAL_SET_VECTOR_STATE)(
+    _In_ ULONG Par1,
+    _In_ ULONG Par2
+);
+
+typedef ULONG
+(NTAPI * PHAL_GET_APIC_VERSION)(
+    _In_ ULONG Par1
+);
+
+typedef VOID
+(NTAPI * PHAL_SET_MAX_LEGACY_PCI_BUS_NUMBER)(
+    _In_ ULONG Par1
+);
+
+typedef BOOLEAN
+(NTAPI * PHAL_IS_VECTOR_VALID)(
+    _In_ ULONG Vector
+);
+
+typedef struct _ACPI_PM_DISPATCH_TABLE
+{
+    ULONG Signature;
+    ULONG Version;
+    PHAL_ACPI_TIMER_INIT HalAcpiTimerInit;                          // HaliAcpiTimerInit
+    PVOID HalAcpiTimerInterrupt;
+    PHAL_ACPI_MACHINE_STATE_INIT HalAcpiMachineStateInit;           // HaliAcpiMachineStateInit
+    PHAL_ACPI_QUERY_FLAGS HalAcpiQueryFlags;                        // HaliAcpiQueryFlags
+    PHAL_ACPI_PIC_STATE_INTACT HalAcpiPicStateIntact;               // HalpAcpiPicStateIntact
+    PHAL_RESTORE_INT_CONTROLLER_STATE HalRestoreIntControllerState; // HalpRestoreInterruptControllerState
+    PHAL_PCI_INTERFACE_READ_CONFIG HalPciInterfaceReadConfig;       // HaliPciInterfaceReadConfig
+    PHAL_PCI_INTERFACE_WRITE_CONFIG HalPciInterfaceWriteConfig;     // HaliPciInterfaceWriteConfig
+    PHAL_SET_VECTOR_STATE HalSetVectorState;                        // HaliSetVectorState
+    PHAL_GET_APIC_VERSION HalGetApicVersion;                        // HalpGetApicVersion
+    PHAL_SET_MAX_LEGACY_PCI_BUS_NUMBER HalSetMaxLegacyPciBusNumber; // HaliSetMaxLegacyPciBusNumber
+    PHAL_IS_VECTOR_VALID HalIsVectorValid;                          // HaliIsVectorValid
+} ACPI_PM_DISPATCH_TABLE, *PACPI_PM_DISPATCH_TABLE;
+
+VOID
+NTAPI
+HaliAcpiMachineStateInit(
+    _In_ ULONG Par1,
+    _In_ PVOID Par2,
+    _Out_ PVOID OutPar3
+);
+
+ULONG
+NTAPI
+HaliAcpiQueryFlags(
+    VOID
+);
+
+UCHAR
+NTAPI
+HalpAcpiPicStateIntact(
+    VOID
+);
+
+VOID
+NTAPI
+HalpRestoreInterruptControllerState(
+    VOID
+);
+
+VOID
+NTAPI
+HaliSetVectorState(
+    _In_ ULONG Par1,
+    _In_ ULONG Par2
+);
+
+ULONG
+NTAPI
+HalpGetApicVersion(
+    _In_ ULONG Par1
+);
+
+VOID
+NTAPI
+HaliSetMaxLegacyPciBusNumber(
+    _In_ ULONG MaxLegacyPciBusNumber
+);
+
+BOOLEAN
+NTAPI
+HaliIsVectorValid(
+    _In_ ULONG Vector
+);
+
+/* EOF */
