5ecc064907b19e320ec2c0273ad6e0a6f7cf0bb5
Vadim Galyant <vgal@rambler.ru>
2022-05-02 14:19:47 +0400

    [NTOS:PNP] Implement IopGetRootDevices().

--- ntoskrnl/io/pnpio.h
+++ ntoskrnl/io/pnpio.h
@@ -1,6 +1,9 @@
 #ifndef _PNPIO_H
 #define _PNPIO_H
 
+#define PIP_SUBKEY_FLAG_SKIP_ERROR  1
+#define PIP_SUBKEY_FLAG_DELETE_KEY  2
+
 /* Dump resources flags */
 #define PIP_DUMP_FL_ALL_NODES        1
 #define PIP_DUMP_FL_RES_ALLOCATED    2
