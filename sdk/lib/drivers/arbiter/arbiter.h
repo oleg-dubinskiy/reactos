cdae83cf341cb7c2e1542e62329ef393efd1c414
Vadim Galyant <vgal@rambler.ru>
2022-05-06 08:56:59 +0400

    [NTOS:PNP] Add ArbArbiterHandler() to arbiter.h.

--- sdk/lib/drivers/arbiter/arbiter.h
+++ sdk/lib/drivers/arbiter/arbiter.h
@@ -241,4 +241,12 @@ ArbInitializeArbiterInstance(
     _In_ PARB_TRANSLATE_ORDERING TranslateOrderingFunction
 );
 
+NTSTATUS
+NTAPI
+ArbArbiterHandler(
+    _In_ PVOID Context,
+    _In_ ARBITER_ACTION Action,
+    _Out_ PARBITER_PARAMETERS Params
+);
+
 /* EOF */
