94772245e350484feecfcbd501b4d0f039e7678b
Vadim Galyant <vgal@rambler.ru>
2022-05-09 11:56:23 +0400

    [HALACPI] Add HalpGetChipHacks().

--- hal/halacpi/acpi/dispatch.c
+++ hal/halacpi/acpi/dispatch.c
@@ -31,6 +31,7 @@ HALP_TIMER_INFO TimerInfo;
 BOOLEAN HalpBrokenAcpiTimer = FALSE;
 
 extern FADT HalpFixedAcpiDescTable;
+extern ULONG HalpWAETDeviceFlags;
 
 /* PRIVATE FUNCTIONS *********************************************************/
 
@@ -88,6 +89,80 @@ HaliAcpiTimerInit(_In_ PULONG TimerPort,
 
     /* FIXME: Now proceed to the timer initialization */
     HalaAcpiTimerInit(TimerPort, TimerValExt);
+}
+
+NTSTATUS
+NTAPI
+HalpGetChipHacks(_In_ USHORT VendorID,
+                 _In_ USHORT DeviceID,
+                 _In_ UCHAR RevisionID,
+                 _Out_ ULONG* OutChipHacks)
+{
+    KEY_VALUE_PARTIAL_INFORMATION KeyValueInfo;
+    OBJECT_ATTRIBUTES ObjectAttributes;
+    UNICODE_STRING DestinationString;
+    HANDLE KeyHandle = NULL;
+    ULONG ResultLength;
+    ULONG ChipHacks;
+    WCHAR SourceString[10];
+    NTSTATUS Status;
+
+    PAGED_CODE();
+    DPRINT("HalpGetChipHacks: VendorID %X, DeviceID %X\n", VendorID, DeviceID);
+
+    RtlInitUnicodeString(&DestinationString, L"\\REGISTRY\\MACHINE\\SYSTEM\\CURRENTCONTROLSET\\Control\\HAL");
+
+    InitializeObjectAttributes(&ObjectAttributes, &DestinationString, OBJ_CASE_INSENSITIVE, NULL, NULL);
+
+    Status = ZwOpenKey(&KeyHandle, KEY_READ, &ObjectAttributes);
+    if (!NT_SUCCESS(Status))
+    {
+        DPRINT1("HalpGetChipHacks: Status %X\n", Status);
+        KeFlushWriteBuffer();
+        return Status;
+    }
+
+    swprintf(SourceString, L"%04X%04X", VendorID, DeviceID);
+    RtlInitUnicodeString(&DestinationString, SourceString);
+
+    Status = ZwQueryValueKey(KeyHandle,
+                             &DestinationString,
+                             KeyValuePartialInformation,
+                             &KeyValueInfo,
+                             sizeof(KeyValueInfo),
+                             &ResultLength);
+    if (!NT_SUCCESS(Status))
+    {
+        ZwClose(KeyHandle);
+        KeFlushWriteBuffer();
+        return Status;
+    }
+
+    ChipHacks = *(PULONG)KeyValueInfo.Data;
+    DPRINT1("HalpGetChipHacks: ChipHacks %X\n", ChipHacks);
+
+    if (RevisionID && (RevisionID >= (ChipHacks >> 24)))
+    {
+        DPRINT1("HalpGetChipHacks: RevisionID %X\n", RevisionID);
+        ASSERT(0); // HalpDbgBreakPointEx();
+        ChipHacks >>= 12;
+    }
+
+    ChipHacks &= 0xFFF;
+
+    if (HalpWAETDeviceFlags & 2)
+    {
+        DPRINT1("HalpGetChipHacks: HalpWAETDeviceFlags %X\n", HalpWAETDeviceFlags);
+        ASSERT(0); // HalpDbgBreakPointEx();
+        ChipHacks &= 0xFFFFFFFE;
+    }
+
+    *OutChipHacks = ChipHacks;
+
+    ZwClose(KeyHandle);
+    KeFlushWriteBuffer();
+
+    return Status;
 }
 
 NTSTATUS
