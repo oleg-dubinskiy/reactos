c016ca0773bd3207b995252e450e91bc0d91eb11
Vadim Galyant <vgal@rambler.ru>
2022-05-29 01:09:46 +0400

    [HALACPI] Add HalpReboot().

--- hal/halacpi/generic/reboot.c
+++ hal/halacpi/generic/reboot.c
@@ -7,6 +7,61 @@
 
 /* PRIVATE FUNCTIONS *********************************************************/
 
+VOID
+NTAPI
+HalpReboot(VOID)
+{
+    PHYSICAL_ADDRESS PhysicalAddress;
+    UCHAR Data;
+    PVOID ZeroPageMapping;
+
+    DPRINT("HalpReboot()\n");
+
+    /* Map the first physical page */
+    PhysicalAddress.QuadPart = 0;
+    ZeroPageMapping = HalpMapPhysicalMemoryWriteThrough64(PhysicalAddress, 1);
+
+    /* Enable warm reboot */
+    ((PUSHORT)ZeroPageMapping)[0x239] = 0x1234;
+
+    /* Lock CMOS Access (and disable interrupts) */
+    HalpAcquireCmosSpinLock();
+
+    /* Setup control register B */
+    WRITE_PORT_UCHAR(CMOS_CONTROL_PORT, 0x0B);
+    KeStallExecutionProcessor(1);
+
+    /* Read periodic register and clear the interrupt enable */
+    Data = READ_PORT_UCHAR(CMOS_DATA_PORT);
+    KeStallExecutionProcessor(1);
+    WRITE_PORT_UCHAR(CMOS_DATA_PORT, Data & ~0x40);
+    KeStallExecutionProcessor(1);
+
+    /* Setup control register A */
+    WRITE_PORT_UCHAR(CMOS_CONTROL_PORT, 0x0A);
+    KeStallExecutionProcessor(1);
+
+    /* Read divider rate and reset it */
+    Data = READ_PORT_UCHAR(CMOS_DATA_PORT);
+    KeStallExecutionProcessor(1);
+    WRITE_PORT_UCHAR(CMOS_DATA_PORT, (Data & ~0x9) | 0x06);
+    KeStallExecutionProcessor(1);
+
+    /* Reset neutral CMOS address */
+    WRITE_PORT_UCHAR(CMOS_CONTROL_PORT, 0x15);
+    KeStallExecutionProcessor(1);
+
+    /* Flush write buffers and send the reset command */
+    KeFlushWriteBuffer();
+
+    DPRINT("HalpReboot: FIXME HalpResetAllProcessors()\n");
+    //HalpResetAllProcessors();
+
+    HalpWriteResetCommand();
+
+    /* Halt the CPU */
+    __halt();
+}
 
 /* PUBLIC FUNCTIONS **********************************************************/
 
