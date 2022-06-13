046f07fc711ee448cedd348da33d7566533b1f09
Vadim Galyant <vgal@rambler.ru>
2022-06-14 00:23:30 +0400

    [HALAACPI] Implement DetectAcpiMP() (full).

--- hal/halaacpi/apic/apic.c
+++ hal/halaacpi/apic/apic.c
@@ -19,6 +19,85 @@ HALP_MP_INFO_TABLE HalpMpInfoTable;
 APIC_INTI_INFO HalpIntiInfo[MAX_INTI];
 USHORT HalpMaxApicInti[MAX_IOAPICS];
 
+UCHAR
+HalpIRQLtoTPR[32] =
+{
+    0x00, /*  0 PASSIVE_LEVEL */
+    0x3d, /*  1 APC_LEVEL */
+    0x41, /*  2 DISPATCH_LEVEL */
+    0x41, /*  3 \  */
+    0x51, /*  4  \ */
+    0x61, /*  5  | */
+    0x71, /*  6  | */
+    0x81, /*  7  | */
+    0x91, /*  8  | */
+    0xa1, /*  9  | */
+    0xb1, /* 10  | */
+    0xb1, /* 11  | */
+    0xb1, /* 12  | */
+    0xb1, /* 13  | */
+    0xb1, /* 14  | */
+    0xb1, /* 15 DEVICE IRQL */
+    0xb1, /* 16  | */
+    0xb1, /* 17  | */
+    0xb1, /* 18  | */
+    0xb1, /* 19  | */
+    0xb1, /* 20  | */
+    0xb1, /* 21  | */
+    0xb1, /* 22  | */
+    0xb1, /* 23  | */
+    0xb1, /* 24  | */
+    0xb1, /* 25  / */
+    0xb1, /* 26 /  */
+    0xc1, /* 27 PROFILE_LEVEL */
+    0xd1, /* 28 CLOCK2_LEVEL */
+    0xe1, /* 29 IPI_LEVEL */
+    0xef, /* 30 POWER_LEVEL */
+    0xff, /* 31 HIGH_LEVEL */
+};
+
+KIRQL
+HalVectorToIRQL[16] =
+{
+       0, /* 00 PASSIVE_LEVEL */
+    0xff, /* 10 */
+    0xff, /* 20 */
+       1, /* 3D APC_LEVEL */
+       2, /* 41 DISPATCH_LEVEL */
+       4, /* 50 \ */
+       5, /* 60  \ */
+       6, /* 70  | */
+       7, /* 80 DEVICE IRQL */
+       8, /* 90  | */
+       9, /* A0  / */
+      10, /* B0 /  */
+      27, /* C1 PROFILE_LEVEL */
+      28, /* D1 CLOCK2_LEVEL */
+      29, /* E1 IPI_LEVEL / EF POWER_LEVEL */
+      31, /* FF HIGH_LEVEL */
+};
+
+/* For 0x50..0xBF vectors IRQLs values saves dynamically in HalpAllocateSystemInterruptVector() */
+KIRQL HalpVectorToIRQL[16] =
+{
+    0x00, /* 00 PASSIVE_LEVEL */
+    0xFF, /* 10 */
+    0xFF, /* 20 */
+    0x01, /* 3D APC_LEVEL */
+    0x02, /* 41 DISPATCH_LEVEL */
+    0xFF, /* 50 \ */
+    0xFF, /* 60  \ */
+    0xFF, /* 70  | */
+    0xFF, /* 80 DEVICE IRQL */
+    0xFF, /* 90  | */
+    0xFF, /* A0  / */
+    0xFF, /* B0 /  */
+    0x1B, /* C1 PROFILE_LEVEL */
+    0x1C, /* D1 CLOCK2_LEVEL */
+    0x1D, /* E1 IPI_LEVEL / EF POWER_LEVEL */
+    0x1F, /* FF HIGH_LEVEL */
+};
+
 /* PRIVATE FUNCTIONS *********************************************************/
 
 
@@ -32,6 +111,12 @@ DetectAcpiMP(
     _In_ PLOADER_PARAMETER_BLOCK LoaderBlock)
 {
     PACPI_TABLE_MADT HalpApicTable;
+    PHYSICAL_ADDRESS PhAddress;
+    ULONG_PTR LocalApicBaseVa;
+    ULONG ix;
+
+    LoaderBlock->Extension->HalpIRQLToTPR = HalpIRQLtoTPR;
+    LoaderBlock->Extension->HalpVectorToIRQL = HalpVectorToIRQL;
 
     RtlZeroMemory(&HalpMpInfoTable, sizeof(HalpMpInfoTable));
 
@@ -51,6 +136,30 @@ DetectAcpiMP(
         return FALSE;
     }
 
+    if (HalpMpInfoTable.ProcessorCount > 1)
+        *OutIsMpSystem = TRUE;
+
+    HalpMpInfoTable.LocalApicPA = HalpApicTable->Address;
+    PhAddress.QuadPart = HalpMpInfoTable.LocalApicPA;
+
+    LocalApicBaseVa = (ULONG_PTR)HalpMapPhysicalMemoryWriteThrough64(PhAddress, 1);
+
+    HalpRemapVirtualAddress64((PVOID)LOCAL_APIC_BASE, PhAddress, 1);
+
+    if (*(volatile PUCHAR)(LocalApicBaseVa + APIC_VER) > 0x1F)
+    {
+        HalDisplayString("HAL: Bad APIC Version");
+        return FALSE;
+    }
+
+    for (ix = 0; ix < HalpMpInfoTable.IoApicCount; ix++)
+    {
+        if (!HalpVerifyIOUnit((PIO_APIC_REGISTERS)HalpMpInfoTable.IoApicVA[ix]))
+        {
+            HalDisplayString("HAL: APIC not verified");
+            return FALSE;
+        }
+    }
 
     HalDisplayString("HAL: DetectAPIC: APIC system found - Returning TRUE\n");
     return TRUE;
--- hal/halaacpi/include/apic.h
+++ hal/halaacpi/include/apic.h
@@ -8,6 +8,46 @@
 #define MAX_INT_VECTORS  256
 #define MAX_INTI         (32 * MAX_IOAPICS)
 
+#ifdef _M_AMD64
+  #define LOCAL_APIC_BASE  0xFFFFFFFFFFFE0000ULL // checkme!
+#else
+  #define LOCAL_APIC_BASE  0xFFFE0000
+#endif
+
+/* APIC Register Address Map */
+#define APIC_ID       0x0020 /* Local APIC ID Register (R/W) */
+#define APIC_VER      0x0030 /* Local APIC Version Register (R) */
+#define APIC_TPR      0x0080 /* Task Priority Register (R/W) */
+#define APIC_APR      0x0090 /* Arbitration Priority Register (R) */
+#define APIC_PPR      0x00A0 /* Processor Priority Register (R) */
+#define APIC_EOI      0x00B0 /* EOI Register (W) */
+#define APIC_RRR      0x00C0 /* Remote Read Register () */
+#define APIC_LDR      0x00D0 /* Logical Destination Register (R/W) */
+#define APIC_DFR      0x00E0 /* Destination Format Register (0-27 R, 28-31 R/W) */
+#define APIC_SIVR     0x00F0 /* Spurious Interrupt Vector Register (0-3 R, 4-9 R/W) */
+#define APIC_ISR      0x0100 /* Interrupt Service Register 0-255 (R) */
+#define APIC_TMR      0x0180 /* Trigger Mode Register 0-255 (R) */
+#define APIC_IRR      0x0200 /* Interrupt Request Register 0-255 (r) */
+#define APIC_ESR      0x0280 /* Error Status Register (R) */
+#define APIC_ICR0     0x0300 /* Interrupt Command Register 0-31 (R/W) */
+#define APIC_ICR1     0x0310 /* Interrupt Command Register 32-63 (R/W) */
+#define APIC_TMRLVTR  0x0320 /* Timer Local Vector Table (R/W) */
+#define	APIC_THRMLVTR 0x0330 /* Thermal Local Vector Table */
+#define APIC_PCLVTR   0x0340 /* Performance Counter Local Vector Table (R/W) */
+#define APIC_LINT0    0x0350 /* LINT0 Local Vector Table (R/W) */
+#define APIC_LINT1    0x0360 /* LINT1 Local Vector Table (R/W) */
+#define APIC_ERRLVTR  0x0370 /* Error Local Vector Table (R/W) */
+#define APIC_TICR     0x0380 /* Initial Count Register for Timer (R/W) */
+#define APIC_TCCR     0x0390 /* Current Count Register for Timer (R) */
+#define APIC_TDCR     0x03E0 /* Timer Divide Configuration Register (R/W) */
+#define APIC_EAFR     0x0400 /* extended APIC Feature register (R/W) */
+#define APIC_EACR     0x0410 /* Extended APIC Control Register (R/W) */
+#define APIC_SEOI     0x0420 /* Specific End Of Interrupt Register (W) */
+#define APIC_EXT0LVTR 0x0500 /* Extended Interrupt 0 Local Vector Table */
+#define APIC_EXT1LVTR 0x0510 /* Extended Interrupt 1 Local Vector Table */
+#define APIC_EXT2LVTR 0x0520 /* Extended Interrupt 2 Local Vector Table */
+#define APIC_EXT3LVTR 0x0530 /* Extended Interrupt 3 Local Vector Table */
+
 typedef struct _HALP_MP_INFO_TABLE
 {
     ULONG LocalApicversion;
