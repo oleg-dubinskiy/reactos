af67a4e5bbca07e95e9b6f8767f6804e05d371b2
Vadim Galyant <vgal@rambler.ru>
2022-06-17 15:37:09 +0400

    [HALAACPI] Add stubs for:     HalAllProcessorsStarted()     HalProcessorIdle()     HalRequestIpi(long)     HalStartNextProcessor(ptr ptr)

---
+++ hal/halaacpi/up/processor.c
@@ -0,0 +1,53 @@
+
+/* INCLUDES ******************************************************************/
+
+#include <hal.h>
+//#define NDEBUG
+#include <debug.h>
+
+/* GLOBALS ********************************************************************/
+
+
+/* PRIVATE FUNCTIONS *********************************************************/
+
+
+/* FUNCTIONS *****************************************************************/
+
+BOOLEAN
+NTAPI
+HalAllProcessorsStarted(VOID)
+{
+    UNIMPLEMENTED;
+    ASSERT(FALSE); // HalpDbgBreakPointEx();
+    return FALSE;
+}
+
+BOOLEAN
+NTAPI
+HalStartNextProcessor(
+    _In_ PLOADER_PARAMETER_BLOCK LoaderBlock,
+    _In_ PKPROCESSOR_STATE ProcessorState)
+{
+    UNIMPLEMENTED;
+    ASSERT(FALSE); // HalpDbgBreakPointEx();
+    return FALSE;
+}
+
+VOID
+NTAPI
+HalProcessorIdle(VOID)
+{
+    UNIMPLEMENTED;
+    ASSERT(FALSE); // HalpDbgBreakPointEx();
+}
+
+VOID
+NTAPI
+HalRequestIpi(
+    _In_ KAFFINITY TargetProcessors)
+{
+    UNIMPLEMENTED;
+    ASSERT(FALSE); // HalpDbgBreakPointEx();
+}
+
+/* EOF */
