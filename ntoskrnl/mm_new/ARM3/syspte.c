68dd08b313f9a4ee79be392edf9be1eb797afc97
Vadim Galyant <vgal@rambler.ru>
2022-07-13 00:57:46 +0400

    [NTOS:MM_NEW] Add MiInitializeSystemPtes().

--- ntoskrnl/mm_new/ARM3/syspte.c
+++ ntoskrnl/mm_new/ARM3/syspte.c
@@ -8,8 +8,12 @@
 
 /* GLOBALS ********************************************************************/
 
+PMMPTE MmSystemPteBase;
 PMMPTE MmSystemPtesStart[MaximumPtePoolTypes];
 PMMPTE MmSystemPtesEnd[MaximumPtePoolTypes];
+MMPTE MmFirstFreeSystemPte[MaximumPtePoolTypes];
+ULONG MmTotalFreeSystemPtes[MaximumPtePoolTypes];
+ULONG MmTotalSystemPtes;
 
 /* FUNCTIONS ******************************************************************/
 
@@ -31,7 +35,38 @@ MiInitializeSystemPtes(
     _In_ ULONG NumberOfPtes,
     _In_ MMSYSTEM_PTE_POOL_TYPE PoolType)
 {
-    UNIMPLEMENTED_DBGBREAK();
+    /* Sanity checks */
+    ASSERT(NumberOfPtes >= 1);
+
+    /* Set the starting and ending PTE addresses for this space */
+    MmSystemPteBase = MI_SYSTEM_PTE_BASE;
+
+    MmSystemPtesStart[PoolType] = StartingPte;
+    MmSystemPtesEnd[PoolType] = (StartingPte + NumberOfPtes - 1);
+
+    DPRINT("MiInitializeSystemPtes: System PTE space for %d (%p - %p)\n",
+           PoolType, MmSystemPtesStart[PoolType], MmSystemPtesEnd[PoolType]);
+
+    /* Clear all the PTEs to start with */
+    RtlZeroMemory(StartingPte, NumberOfPtes * sizeof(MMPTE));
+
+    /* Make the first entry free and link it */
+    StartingPte->u.List.NextEntry = MM_EMPTY_PTE_LIST;
+    MmFirstFreeSystemPte[PoolType].u.Long = 0;
+    MmFirstFreeSystemPte[PoolType].u.List.NextEntry = (StartingPte - MmSystemPteBase);
+
+    /* The second entry stores the size of this PTE space */
+    StartingPte++;
+    StartingPte->u.Long = 0;
+    StartingPte->u.List.NextEntry = NumberOfPtes;
+
+    /* We also keep a global for it */
+    MmTotalFreeSystemPtes[PoolType] = NumberOfPtes;
+
+    /* Check if this is the system PTE space */
+    if (PoolType == SystemPteSpace)
+        /* Remember how many PTEs we have */
+        MmTotalSystemPtes = NumberOfPtes;
 }
 
 /* EOF */
