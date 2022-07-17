1b0128e94c030522f96078d8504c3aa077fb92b1
Vadim Galyant <vgal@rambler.ru>
2022-07-17 11:36:04 +0400

    [NTOS:MM_NEW] Add MiUpdateThunks().

--- ntoskrnl/mm_new/ARM3/sysldr.c
+++ ntoskrnl/mm_new/ARM3/sysldr.c
@@ -102,6 +102,106 @@ MmCheckSystemImage(
     return STATUS_NOT_IMPLEMENTED;
 }
 
+INIT_FUNCTION
+VOID
+NTAPI
+MiUpdateThunks(
+    _In_ PLOADER_PARAMETER_BLOCK LoaderBlock,
+    _In_ PVOID OldBase,
+    _In_ PVOID NewBase,
+    _In_ ULONG Size)
+{
+    PIMAGE_IMPORT_DESCRIPTOR ImportDescriptor;
+    PLDR_DATA_TABLE_ENTRY LdrEntry;
+    PLIST_ENTRY NextEntry;
+    PULONG_PTR ImageThunk;
+    ULONG_PTR OldBaseTop;
+    ULONG_PTR Delta;
+    ULONG ImportSize;
+    /* FIXME: MINGW-W64 must fix LD to generate drivers that Windows can load,
+       since a real version of Windows would fail at this point,
+       but they seem busy implementing features such as "HotPatch" support in GCC 4.6 instead,
+       a feature which isn't even used by Windows. Priorities, priorities...
+
+       Please note that Microsoft WDK EULA and license prohibits using the information
+       contained within it for the generation of "non-Windows" drivers, which is precisely what LD will generate,
+       since an LD driver will not load on Windows.
+    */
+  #ifdef _WORKING_LINKER_
+    ULONG ix;
+  #endif
+
+    /* Calculate the top and delta */
+    OldBaseTop = ((ULONG_PTR)OldBase + Size - 1);
+    Delta = ((ULONG_PTR)NewBase - (ULONG_PTR)OldBase);
+
+    /* Loop the loader block */
+    for (NextEntry = LoaderBlock->LoadOrderListHead.Flink;
+         NextEntry != &LoaderBlock->LoadOrderListHead;
+         NextEntry = NextEntry->Flink)
+    {
+        /* Get the loader entry */
+        LdrEntry = CONTAINING_RECORD(NextEntry, LDR_DATA_TABLE_ENTRY, InLoadOrderLinks);
+
+      #ifdef _WORKING_LINKER_
+
+        /* Get the IAT */
+        ImageThunk = RtlImageDirectoryEntryToData(LdrEntry->DllBase, TRUE, IMAGE_DIRECTORY_ENTRY_IAT, &ImportSize);
+        if (!ImageThunk)
+            continue;
+
+        /* Make sure we have an IAT */
+        DPRINT("[Mm0]: Updating thunks in: %wZ\n", &LdrEntry->BaseDllName);
+
+        for (ix = 0; ix < ImportSize; ix++, ImageThunk++)
+        {
+            /* Check if it's within this module */
+            if ((*ImageThunk < (ULONG_PTR)OldBase) || (*ImageThunk > OldBaseTop))
+                continue;
+
+            /* Relocate it */
+            DPRINT("[Mm0]: Updating IAT at: %p. Old Entry: %p. New Entry: %p.\n",
+                   ImageThunk, *ImageThunk, *ImageThunk + Delta);
+
+            *ImageThunk += Delta;
+        }
+
+      #else
+
+        /* Get the import table */
+        ImportDescriptor = RtlImageDirectoryEntryToData(LdrEntry->DllBase, TRUE, IMAGE_DIRECTORY_ENTRY_IMPORT, &ImportSize);
+        if (!ImportDescriptor)
+            continue;
+
+        /* Make sure we have an IAT */
+        //DPRINT("[Mm0]: Updating thunks in: %wZ\n", &LdrEntry->BaseDllName);
+
+        while ((ImportDescriptor->Name) && (ImportDescriptor->OriginalFirstThunk))
+        {
+            /* Get the image thunk */
+            for (ImageThunk = (PVOID)((ULONG_PTR)LdrEntry->DllBase + ImportDescriptor->FirstThunk);
+                 *ImageThunk;
+                 ImageThunk++)
+            {
+                /* Check if it's within this module */
+                if ((*ImageThunk < (ULONG_PTR)OldBase) || (*ImageThunk > OldBaseTop))
+                    continue;
+
+               /* Relocate it */
+               DPRINT("[Mm0]: Updating IAT at: %p. Old Entry: %p. New Entry: %p.\n",
+                       ImageThunk, *ImageThunk, *ImageThunk + Delta);
+
+               *ImageThunk += Delta;
+            }
+
+            /* Go to the next import */
+            ImportDescriptor++;
+        }
+
+      #endif
+    }
+}
+
 /* PUBLIC FUNCTIONS ***********************************************************/
 
 PVOID
