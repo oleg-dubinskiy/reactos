a9a74dd50fbe7210f5d23a19fceed828bbbfc329
Vadim Galyant <vgal@rambler.ru>
2022-07-18 10:38:14 +0400

    [NTOS:MM_NEW] Add MiDbgTranslatePhysicalAddress().

--- ntoskrnl/mm_new/ARM3/mmdbg.c
+++ ntoskrnl/mm_new/ARM3/mmdbg.c
@@ -4,6 +4,7 @@
 #include <ntoskrnl.h>
 //#define NDEBUG
 #include <debug.h>
+#include "miarm.h"
 
 /* GLOBALS ********************************************************************/
 
@@ -11,6 +12,63 @@ PVOID MiDebugMapping = MI_DEBUG_MAPPING;
 PMMPTE MmDebugPte = NULL;
 
 /* FUNCTIONS ******************************************************************/
+
+PVOID
+NTAPI
+MiDbgTranslatePhysicalAddress(
+    _In_ ULONG64 PhysicalAddress,
+    _In_ ULONG Flags)
+{
+    PFN_NUMBER Pfn;
+    MMPTE TempPte;
+    PVOID MappingBaseAddress;
+
+    /* Check if we are called too early */
+    if (MmDebugPte == NULL)
+    {
+        /* The structures we require aren't initialized yet, fail */
+        KdpDprintf("MiDbgTranslatePhysicalAddress called too early! Address: 0x%I64x\n", PhysicalAddress);
+        return NULL;
+    }
+
+    /* FIXME: No support for cache flags yet */
+    if ((Flags & (MMDBG_COPY_CACHED | MMDBG_COPY_UNCACHED | MMDBG_COPY_WRITE_COMBINED)) != 0)
+    {
+        /* Fail */
+        KdpDprintf("MiDbgTranslatePhysicalAddress: Cache flags not yet supported. Flags: 0x%lx\n",
+                   (Flags & (MMDBG_COPY_CACHED | MMDBG_COPY_UNCACHED | MMDBG_COPY_WRITE_COMBINED)));
+        return NULL;
+    }
+
+    /* Save the base address of our mapping page */
+    MappingBaseAddress = MiPteToAddress(MmDebugPte);
+
+    /* Get the template */
+    TempPte = ValidKernelPte;
+
+    /* Convert physical address to PFN */
+    Pfn = (PFN_NUMBER)(PhysicalAddress >> PAGE_SHIFT);
+
+    /* Check if this could be an I/O mapping */
+    if (!MiGetPfnEntry(Pfn))
+    {
+        /* FIXME: We don't support this yet */
+        KdpDprintf("MiDbgTranslatePhysicalAddress: I/O Space not yet supported. PFN: 0x%I64x\n", (ULONG64)Pfn);
+        return NULL;
+    }
+    else
+    {
+        /* Set the PFN in the PTE */
+        TempPte.u.Hard.PageFrameNumber = Pfn;
+    }
+
+    /* Map the PTE and invalidate its TLB entry */
+    *MmDebugPte = TempPte;
+    KeInvalidateTlbEntry(MappingBaseAddress);
+
+    /* Calculate and return the virtual offset into our mapping page */
+    return (PVOID)((ULONG_PTR)MappingBaseAddress + BYTE_OFFSET(PhysicalAddress));
+}
 
 NTSTATUS
 NTAPI
