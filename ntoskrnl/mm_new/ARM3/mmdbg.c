078907be0c9e65d1851b762acc634909150a56bc
Vadim Galyant <vgal@rambler.ru>
2022-07-18 10:40:27 +0400

    [NTOS:MM_NEW] Add MmDbgCopyMemory().

--- ntoskrnl/mm_new/ARM3/mmdbg.c
+++ ntoskrnl/mm_new/ARM3/mmdbg.c
@@ -85,6 +85,9 @@ MiDbgUnTranslatePhysicalAddress(VOID)
     KeInvalidateTlbEntry(MappingBaseAddress);
 }
 
+/* We handle 8-byte requests at most */
+C_ASSERT(MMDBG_COPY_MAX_SIZE == 8);
+
 NTSTATUS
 NTAPI
 MmDbgCopyMemory(
@@ -93,8 +96,139 @@ MmDbgCopyMemory(
     _In_ ULONG Size,
     _In_ ULONG Flags)
 {
-    UNIMPLEMENTED_DBGBREAK();
-    return STATUS_NOT_IMPLEMENTED;
+    PVOID TargetAddress;
+    ULONG64 PhysicalAddress;
+    PMMPTE Pte;
+    PVOID CopyDestination;
+    PVOID CopySource;
+
+    /* No local kernel debugging support yet, so don't worry about locking */
+    ASSERT(Flags & MMDBG_COPY_UNSAFE);
+
+    /* We only handle 1, 2, 4 and 8 byte requests */
+    if ((Size != 1) &&
+        (Size != 2) &&
+        (Size != 4) &&
+        (Size != 8))
+    {
+        /* Invalid size, fail */
+        KdpDprintf("MmDbgCopyMemory: Received Illegal Size 0x%lx\n", Size);
+        return STATUS_INVALID_PARAMETER_3;
+    }
+
+    /* The copy must be aligned */
+    if ((Address & (Size - 1)) != 0)
+    {
+        /* Not allowing unaligned access */
+        KdpDprintf("MmDbgCopyMemory: Received Unaligned Address 0x%I64x Size %lx\n", Address, Size);
+        return STATUS_INVALID_PARAMETER_3;
+    }
+
+    /* Check for physical or virtual copy */
+    if (Flags & MMDBG_COPY_PHYSICAL)
+    {
+        /* Physical: translate and map it to our mapping space */
+        TargetAddress = MiDbgTranslatePhysicalAddress(Address, Flags);
+
+        /* Check if translation failed */
+        if (!TargetAddress)
+        {
+            /* Fail */
+            KdpDprintf("MmDbgCopyMemory: Failed to Translate Physical Address %I64x\n", Address);
+            return STATUS_UNSUCCESSFUL;
+        }
+
+        /* The address we received must be valid! */
+        ASSERT(MmIsAddressValid(TargetAddress));
+    }
+    else
+    {
+        /* Virtual; truncate it to avoid casts later down */
+        TargetAddress = (PVOID)(ULONG_PTR)Address;
+
+        /* Make sure address is valid */
+        if (!MmIsAddressValid(TargetAddress))
+        {
+            /* Fail */
+            KdpDprintf("MmDbgCopyMemory: Failing %s for invalid Virtual Address 0x%p\n",
+                       (Flags & MMDBG_COPY_WRITE ? "write" : "read"), TargetAddress);
+
+            return STATUS_UNSUCCESSFUL;
+        }
+
+        /* Not handling session space correctly yet */
+        if (MmIsSessionAddress(TargetAddress))
+            ;/* FIXME */
+
+        /* If we are going to write to the address, then check if its writable */
+        Pte = MiAddressToPte(TargetAddress);
+
+        if ((Flags & MMDBG_COPY_WRITE) && (!MI_IS_PAGE_WRITEABLE(Pte)))
+        {
+            /* Not writable, we need to do a physical copy */
+            Flags |= MMDBG_COPY_PHYSICAL;
+
+            /* Calculate the physical address */
+            PhysicalAddress = (Pte->u.Hard.PageFrameNumber << PAGE_SHIFT);
+            PhysicalAddress += BYTE_OFFSET(Address);
+
+            /* Translate the physical address */
+            TargetAddress = MiDbgTranslatePhysicalAddress(PhysicalAddress, Flags);
+
+            /* Check if translation failed */
+            if (!TargetAddress)
+            {
+                /* Fail */
+                KdpDprintf("MmDbgCopyMemory: Failed to translate for write %I64x (%I64x)\n", PhysicalAddress, Address);
+                return STATUS_UNSUCCESSFUL;
+            }
+        }
+    }
+
+    /* Check what kind of operation this is */
+    if (Flags & MMDBG_COPY_WRITE)
+    {
+        /* Write */
+        CopyDestination = TargetAddress;
+        CopySource = Buffer;
+    }
+    else
+    {
+        /* Read */
+        CopyDestination = Buffer;
+        CopySource = TargetAddress;
+    }
+
+    /* Do the copy  */
+    switch (Size)
+    {
+        case 1: /* 1 byte */
+            *(PUCHAR)CopyDestination = *(PUCHAR)CopySource;
+            break;
+
+        case 2: /* 2 bytes */
+            *(PUSHORT)CopyDestination = *(PUSHORT)CopySource;
+            break;
+
+        case 4: /* 4 bytes */
+            *(PULONG)CopyDestination = *(PULONG)CopySource;
+            break;
+
+        case 8: /* 8 bytes */
+            *(PULONGLONG)CopyDestination = *(PULONGLONG)CopySource;
+            break;
+
+        /* Size is sanitized above */
+        DEFAULT_UNREACHABLE;
+    }
+
+    /* Get rid of the mapping if this was a physical copy */
+    if (Flags & MMDBG_COPY_PHYSICAL)
+        /* Unmap and flush it */
+        MiDbgUnTranslatePhysicalAddress();
+
+    /* And we are done */
+    return STATUS_SUCCESS;
 }
 
 /* EOF */
