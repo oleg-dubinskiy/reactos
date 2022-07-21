0147f85ac0063fd58849e0f107e6c233e06ce92f
Vadim Galyant <vgal@rambler.ru>
2022-07-21 10:48:01 +0400

    [NTOS:MM_NEW] Add MiResolveDemandZeroFault().

--- ntoskrnl/mm_new/ARM3/pagfault.c
+++ ntoskrnl/mm_new/ARM3/pagfault.c
@@ -8,15 +8,21 @@
 
 /* GLOBALS ********************************************************************/
 
+#define HYDRA_PROCESS (PEPROCESS)1
+
 ULONG MmDataClusterSize;
 ULONG MmCodeClusterSize;
 ULONG MmInPageSupportMinimum = 4;
 
 extern PMMPTE MiSessionLastPte;
 extern PMM_SESSION_SPACE MmSessionSpace;
-#if (_MI_PAGING_LEVELS <= 3)
-  extern PMMPDE MmSystemPagePtes;
-#endif
+extern PVOID MmHyperSpaceEnd;
+extern PFN_NUMBER MmAvailablePages;
+extern PVOID MiSessionImageStart;
+extern PVOID MiSessionImageEnd;
+extern PVOID MiSessionViewStart;   // 0xBE000000
+extern PVOID MiSessionSpaceWs;
+extern ULONG MmSecondaryColorMask;
 
 /* FUNCTIONS ******************************************************************/
 
@@ -36,6 +42,160 @@ MmSetExecuteOptions(
 {
     UNIMPLEMENTED_DBGBREAK();
     return STATUS_NOT_IMPLEMENTED;
+}
+
+BOOLEAN
+NTAPI
+MiSetDirtyBit(
+    _In_ PVOID Address,
+    _In_ PMMPTE Pte,
+    _In_ BOOLEAN Param3)
+{
+    UNIMPLEMENTED_DBGBREAK();
+    return FALSE;
+}
+
+NTSTATUS
+NTAPI
+MiResolveDemandZeroFault(
+    _In_ PVOID Address,
+    _In_ PMMPTE Pte,
+    _In_ ULONG Protection,
+    _In_ PEPROCESS Process,
+    _In_ KIRQL OldIrql)
+{
+    PFN_NUMBER PageFrameNumber;
+    MMPTE TempPte;
+    PMMPFN Pfn;
+    ULONG Color;
+    BOOLEAN IsNeedAnyPage = FALSE;
+    BOOLEAN HaveLock = FALSE;
+    BOOLEAN IsZeroPage = FALSE;
+
+    DPRINT("MiResolveDemandZeroFault: (%p) Pte %p [%I64X], %X, %p, %X\n",
+           Address, Pte, MiGetPteContents(Pte), Protection, Process, OldIrql);
+
+    /* Must currently only be called by paging path */
+    if (Process > HYDRA_PROCESS && OldIrql == MM_NOIRQL)
+    {
+        ASSERT(FALSE);
+    }
+    else
+    {
+        /* Check if we not need a zero page */
+        if (OldIrql == MM_NOIRQL)
+        {
+            /* Non session-backed image views must be not zeroed */
+            if (Process != HYDRA_PROCESS ||
+                (!MI_IS_SESSION_IMAGE_ADDRESS(Address) &&
+                 (Address < (PVOID)MiSessionViewStart || Address >= (PVOID)MiSessionSpaceWs)))
+            {
+                IsNeedAnyPage = TRUE;
+            }
+        }
+
+        /* Hardcode unknown color */
+        Color = 0xFFFFFFFF;
+    }
+
+    /* Check if the PFN database should be acquired */
+    if (OldIrql == MM_NOIRQL)
+    {
+        /* Acquire it and remember we should release it after */
+        OldIrql = MiLockPfnDb(APC_LEVEL);
+        HaveLock = TRUE;
+    }
+
+    /* We either manually locked the PFN DB, or already came with it locked */
+    MI_ASSERT_PFN_LOCK_HELD();
+    ASSERT (Pte->u.Hard.Valid == 0);
+
+    /* Assert we have enough pages */
+    if (MmAvailablePages < 0x80) // FIMXE MiEnsureAvailablePageOrWait()
+    {
+        if (HaveLock)
+            MiUnlockPfnDb(OldIrql, APC_LEVEL);
+
+        return 0xC7303001;
+    }
+
+    /* Do we need a zero page? */
+    if (Color != 0xFFFFFFFF)
+    {
+        ASSERT(FALSE);
+    }
+    else
+    {
+        /* Get a color, and see if we should grab a zero or non-zero page */
+      #if !defined(CONFIG_SMP)
+        Color = MI_GET_NEXT_COLOR();
+      #else
+        //#error FIXME
+        Color = MI_GET_NEXT_COLOR();
+      #endif
+
+        if (IsNeedAnyPage)
+            /* Process or system doesn't want a zero page, grab anything */
+            PageFrameNumber = MiRemoveAnyPage(Color);
+        else
+            /* System wants a zero page, obtain one */
+            PageFrameNumber = MiRemoveZeroPage(Color);
+    }
+
+    /* Initialize it */
+    MiInitializePfn(PageFrameNumber, Pte, TRUE);
+
+    /* Do we have the lock? */
+    if (HaveLock)
+    {
+        /* Release it */
+            MiUnlockPfnDb(OldIrql, APC_LEVEL);
+
+        /* Update performance counters */
+        if (Process > HYDRA_PROCESS)
+            Process->NumberOfPrivatePages++;
+    }
+
+    /* Increment demand zero faults */
+    InterlockedIncrement(&KeGetCurrentPrcb()->MmDemandZeroCount);
+
+    /* Get the PFN entry */
+    Pfn = MI_PFN_ELEMENT(PageFrameNumber);
+
+    /* Zero the page if need be */
+    if (IsZeroPage)
+    {
+        ASSERT(FALSE);
+    }
+
+    /* Fault on user PDE, or fault on user PTE? */
+    if (Pte <= MiHighestUserPte)
+        /* User fault, build a user PTE */
+        MI_MAKE_HARDWARE_PTE_USER(&TempPte, Pte, Pte->u.Soft.Protection, PageFrameNumber);
+    else
+        /* This is a user-mode PDE, create a kernel PTE for it */
+        MI_MAKE_HARDWARE_PTE(&TempPte, Pte, Pte->u.Soft.Protection, PageFrameNumber);
+
+    /* Set it dirty if it's a writable page */
+    if (MI_IS_PAGE_WRITEABLE(&TempPte))
+        MI_MAKE_DIRTY_PAGE(&TempPte);
+
+    /* Write it */
+    MI_WRITE_VALID_PTE(Pte, TempPte);
+
+    /* Did we manually acquire the lock */
+    if (HaveLock)
+    {
+        /* Windows does these sanity checks */
+        ASSERT(Pfn->u1.Event == 0);
+        ASSERT(Pfn->u3.e1.PrototypePte == 0);
+
+        DPRINT("MiResolveDemandZeroFault: FIXME MiAddValidPageToWorkingSet()\n");
+    }
+
+    /* It's all good now */
+    DPRINT("MiResolveDemandZeroFault: Demand zero page has now been paged in\n");
+    return STATUS_PAGE_FAULT_DEMAND_ZERO;
 }
 
 NTSTATUS
