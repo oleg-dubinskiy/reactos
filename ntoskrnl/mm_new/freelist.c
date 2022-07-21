f5d8dcc3ff22132e5d9333a9c9903de53d59030b
Vadim Galyant <vgal@rambler.ru>
2022-07-21 09:11:08 +0400

    [NTOS:MM_NEW] Add MiIsPfnFree() and MiIsPfnInUse().

--- ntoskrnl/mm_new/freelist.c
+++ ntoskrnl/mm_new/freelist.c
@@ -4,6 +4,7 @@
 #include <ntoskrnl.h>
 //#define NDEBUG
 #include <debug.h>
+#include "ARM3/miarm.h"
 
 /* GLOBALS ********************************************************************/
 
@@ -22,6 +23,27 @@ SIZE_T MmPeakCommitment;
 SIZE_T MmtotalCommitLimitMaximum;
 
 /* FUNCTIONS ******************************************************************/
+
+BOOLEAN
+NTAPI
+MiIsPfnFree(
+    _In_ PMMPFN Pfn)
+{
+    /* Must be a free or zero page, with no references, linked */
+    return ((Pfn->u3.e1.PageLocation <= StandbyPageList) &&
+            (Pfn->u1.Flink) &&
+            (Pfn->u2.Blink) &&
+            !(Pfn->u3.e2.ReferenceCount));
+}
+
+BOOLEAN
+NTAPI
+MiIsPfnInUse(
+    _In_ PMMPFN Pfn)
+{
+    /* Standby list or higher, unlinked, and with references */
+    return !MiIsPfnFree(Pfn);
+}
 
 
 /* EOF */
