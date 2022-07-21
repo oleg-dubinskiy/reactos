c08b1fb8854c2fff4bc4c2701e76b29a5673f447
Vadim Galyant <vgal@rambler.ru>
2022-07-22 01:11:54 +0400

    [NTOS:MM_NEW] Add MmInitPagingFile().

--- ntoskrnl/mm_new/pagefile.c
+++ ntoskrnl/mm_new/pagefile.c
@@ -4,13 +4,37 @@
 #include <ntoskrnl.h>
 //#define NDEBUG
 #include <debug.h>
+#include "ARM3/miarm.h"
 
 /* GLOBALS ********************************************************************/
+
+#define MAX_PAGING_FILES (0x10)
+
+/* Page file information */
+typedef struct _MMPAGING_FILE
+{
+    PFN_NUMBER Size;
+    PFN_NUMBER MaximumSize;
+    PFN_NUMBER MinimumSize;
+    PFN_NUMBER FreeSpace;
+    PFN_NUMBER CurrentUsage;
+    PFILE_OBJECT FileObject;
+    UNICODE_STRING PageFileName;
+    PRTL_BITMAP Bitmap;
+    HANDLE FileHandle;
+}
+MMPAGING_FILE, *PMMPAGING_FILE;
+
+PMMPAGING_FILE MmPagingFile[MAX_PAGING_FILES]; /* List of paging files, both used and free */
+KGUARDED_MUTEX MmPageFileCreationLock;         /* Lock for examining the list of paging files */
 
 ULONG MmNumberOfPagingFiles; /* Number of paging files */
 PFN_COUNT MiFreeSwapPages;   /* Number of pages that are available for swapping */
 PFN_COUNT MiUsedSwapPages;   /* Number of pages that have been allocated for swapping */
 BOOLEAN MmZeroPageFile;
+
+/* Number of pages that have been reserved for swapping but not yet allocated */
+static PFN_COUNT MiReservedSwapPages;
 
 /* FUNCTIONS ******************************************************************/
 
@@ -21,6 +45,26 @@ MmIsFileObjectAPagingFile(PFILE_OBJECT FileObject)
     UNIMPLEMENTED_DBGBREAK();
     return FALSE;
 }
+
+INIT_FUNCTION
+VOID
+NTAPI
+MmInitPagingFile(VOID)
+{
+    ULONG ix;
+
+    KeInitializeGuardedMutex(&MmPageFileCreationLock);
+
+    MiFreeSwapPages = 0;
+    MiUsedSwapPages = 0;
+    MiReservedSwapPages = 0;
+
+    for (ix = 0; ix < MAX_PAGING_FILES; ix++)
+        MmPagingFile[ix] = NULL;
+
+    MmNumberOfPagingFiles = 0;
+}
+
 
 /* SYSTEM CALLS ***************************************************************/
 
