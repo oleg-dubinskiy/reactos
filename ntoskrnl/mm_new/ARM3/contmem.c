d28ed4a66596a6eb9b3acec0d9bdd802da1eb402
Vadim Galyant <vgal@rambler.ru>
2022-07-21 08:54:26 +0400

    [NTOS:MM_NEW] Add MiFindContiguousPages().

--- ntoskrnl/mm_new/ARM3/contmem.c
+++ ntoskrnl/mm_new/ARM3/contmem.c
@@ -105,6 +105,164 @@ MiCheckForContiguousMemory(
     return NULL;
 }
 
+PFN_NUMBER
+NTAPI
+MiFindContiguousPages(
+    _In_ PFN_NUMBER LowestPfn,
+    _In_ PFN_NUMBER HighestPfn,
+    _In_ PFN_NUMBER BoundaryPfn,
+    _In_ PFN_NUMBER SizeInPages,
+    _In_ MEMORY_CACHING_TYPE CacheType)
+{
+    PMMPFN Pfn;
+    PMMPFN EndPfn;
+    PFN_NUMBER Page;
+    PFN_NUMBER PageCount;
+    PFN_NUMBER LastPage;
+    PFN_NUMBER Length;
+    PFN_NUMBER BoundaryMask;
+    ULONG ix = 0;
+    KIRQL OldIrql;
+
+    PAGED_CODE();
+    ASSERT(SizeInPages != 0);
+
+    /* Convert the boundary PFN into an alignment mask */
+    BoundaryMask = ~(BoundaryPfn - 1);
+
+    /* Disable APCs */
+    KeEnterGuardedRegion();
+
+    /* Loop all the physical memory blocks */
+    do
+    {
+        /* Capture the base page and length of this memory block */
+        Page = MmPhysicalMemoryBlock->Run[ix].BasePage;
+        PageCount = MmPhysicalMemoryBlock->Run[ix].PageCount;
+
+        /* Check how far this memory block will go */
+        LastPage = (Page + PageCount);
+
+        /* Trim it down to only the PFNs we're actually interested in */
+        if ((LastPage - 1) > HighestPfn)
+            LastPage = (HighestPfn + 1);
+
+        if (Page < LowestPfn)
+            Page = LowestPfn;
+
+        /* Skip this run if it's empty or fails to contain all the pages we need */
+        if (!PageCount || (Page + SizeInPages) > LastPage)
+            continue;
+
+        /* Now scan all the relevant PFNs in this run */
+        Length = 0;
+
+        for (Pfn = MI_PFN_ELEMENT(Page); Page < LastPage; Page++, Pfn++)
+        {
+            /* If this PFN is in use, ignore it */
+            if (MiIsPfnInUse(Pfn))
+            {
+                Length = 0;
+                continue;
+            }
+
+            /* If we haven't chosen a start PFN yet and the caller specified an alignment,
+               make sure the page matches the alignment restriction
+            */
+            if (!Length && BoundaryPfn && ((Page ^ (Page + SizeInPages - 1)) & BoundaryMask))
+                /* It does not, so bail out */
+                continue;
+
+            /* Increase the number of valid pages, and check if we have enough */
+            if (++Length != SizeInPages)
+                continue;
+
+            /* It appears we've amassed enough legitimate pages, rollback */
+            Pfn -= (Length - 1);
+            Page -= (Length - 1);
+
+            /* Acquire the PFN lock */
+            OldIrql = MiLockPfnDb(APC_LEVEL);
+
+            /* Things might've changed for us. Is the page still free? */
+            for (; !MiIsPfnInUse(Pfn); Pfn++, Page++)
+            {
+                /* So far so good. Is this the last confirmed valid page? */
+                if (--Length)
+                {
+                    /* Keep going.
+                       The purpose of this loop is to reconfirm
+                       that after acquiring the PFN lock these pages are still usable.
+                    */
+                    continue;
+                }
+
+                /* Sanity check that we didn't go out of bounds */
+                ASSERT(ix != MmPhysicalMemoryBlock->NumberOfRuns);
+
+                /* Loop until all PFN entries have been processed */
+                EndPfn = (Pfn - SizeInPages + 1);
+                do
+                {
+                    /* This PFN is now a used page, set it up */
+                    MI_SET_USAGE(MI_USAGE_CONTINOUS_ALLOCATION);
+                    MI_SET_PROCESS2("Kernel Driver");
+
+                    MiUnlinkFreeOrZeroedPage(Pfn);
+
+                    Pfn->u3.e2.ReferenceCount = 1;
+                    Pfn->u2.ShareCount = 1;
+                    Pfn->u3.e1.PageLocation = ActiveAndValid;
+                    Pfn->u3.e1.StartOfAllocation = 0;
+                    Pfn->u3.e1.EndOfAllocation = 0;
+                    Pfn->u3.e1.PrototypePte = 0;
+                    Pfn->u4.VerifierAllocation = 0;
+                    Pfn->PteAddress = (PVOID)(ULONG_PTR)0xBAADF00DBAADF00DULL;
+
+                    /* Check if this is the last PFN, otherwise go on */
+                    if (Pfn == EndPfn)
+                        break;
+
+                    Pfn--;
+                }
+                while (TRUE);
+
+                /* Mark the first and last PFN so we can find them later */
+                Pfn->u3.e1.StartOfAllocation = 1;
+                (Pfn + SizeInPages - 1)->u3.e1.EndOfAllocation = 1;
+
+                /* Now it's safe to let go of the PFN lock */
+                MiUnlockPfnDb(OldIrql, APC_LEVEL);
+
+                /* Quick sanity check that the last PFN is consistent */
+                EndPfn = (Pfn + SizeInPages);
+                ASSERT(EndPfn == MI_PFN_ELEMENT(Page + 1));
+
+                /* Compute the first page, and make sure it's consistent */
+                Page = (Page - SizeInPages + 1);
+
+                ASSERT(Pfn == MI_PFN_ELEMENT(Page));
+                ASSERT(Page != 0);
+
+                /* Enable APCs and return the page */
+                KeLeaveGuardedRegion();
+                return Page;
+            }
+
+            /* If we got here, something changed while we hadn't acquired the PFN lock yet,
+               so we'll have to restart.
+            */
+            MiUnlockPfnDb(OldIrql, APC_LEVEL);
+            Length = 0;
+        }
+    }
+    while (++ix != MmPhysicalMemoryBlock->NumberOfRuns);
+
+    /* And if we get here, it means no suitable physical memory runs were found */
+    KeLeaveGuardedRegion();
+    return 0;
+}
+
 /* PUBLIC FUNCTIONS ***********************************************************/
 
 PVOID
