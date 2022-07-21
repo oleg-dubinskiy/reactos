ece98f6d536caabf7ecb95bb5ada303781e1bf5f
Vadim Galyant <vgal@rambler.ru>
2022-07-22 00:43:25 +0400

    [NTOS:MM_NEW] Add MiDbgDumpAddressSpace().

--- ntoskrnl/mm_new/mminit.c
+++ ntoskrnl/mm_new/mminit.c
@@ -4,11 +4,85 @@
 #include <ntoskrnl.h>
 //#define NDEBUG
 #include <debug.h>
+#include "ARM3/miarm.h"
 
 /* GLOBALS ********************************************************************/
 
+PMMSUPPORT MmKernelAddressSpace;
+PMMPTE MmSharedUserDataPte;
+
+static GENERIC_MAPPING MmpSectionMapping =
+{
+    STANDARD_RIGHTS_READ | SECTION_MAP_READ | SECTION_QUERY,
+    STANDARD_RIGHTS_WRITE | SECTION_MAP_WRITE,
+    STANDARD_RIGHTS_EXECUTE | SECTION_MAP_EXECUTE,
+    SECTION_ALL_ACCESS
+};
+
+extern PVOID MmNonPagedSystemStart;
+extern PVOID MmNonPagedPoolStart;
+extern PVOID MmNonPagedPoolExpansionStart;
+extern PVOID MmSystemCacheStart;
+extern PVOID MmSystemCacheEnd;
+extern PVOID MiSystemViewStart;
+extern SIZE_T MmBootImageSize;
+extern SIZE_T MmSystemViewSize;
+extern SIZE_T MmSizeOfNonPagedPoolInBytes;
+extern SIZE_T MmSizeOfPagedPoolInBytes;
+extern ULONG_PTR MxPfnAllocation;
+
+extern MM_AVL_TABLE MmSectionBasedRoot;
+extern KGUARDED_MUTEX MmSectionBasedMutex;
+extern PMMPTE MmSharedUserDataPte;
+extern MMPTE ValidKernelPte;
 
 /* FUNCTIONS ******************************************************************/
+
+INIT_FUNCTION
+VOID
+NTAPI
+MiDbgDumpAddressSpace(VOID)
+{
+    ULONG_PTR _BootImageEnd        = ((ULONG_PTR)KSEG0_BASE                   +            MmBootImageSize);
+    SIZE_T    _PfnDatabaseSize     = (MxPfnAllocation << PAGE_SHIFT);
+    ULONG_PTR _PfnDatabaseEnd      = ((ULONG_PTR)MmPfnDatabase                +           _PfnDatabaseSize);
+    ULONG_PTR _NPagedPoolEnd       = ((ULONG_PTR)MmNonPagedPoolStart          +            MmSizeOfNonPagedPoolInBytes);
+    ULONG_PTR _SystemViewEnd       = ((ULONG_PTR)MiSystemViewStart            +            MmSystemViewSize);
+    SIZE_T    _SessionSize         = ((ULONG_PTR)MiSessionSpaceEnd            - (ULONG_PTR)MmSessionBase);
+    SIZE_T    _PageTablesSize      = ((ULONG_PTR)PTE_TOP                      - (ULONG_PTR)PTE_BASE);
+    SIZE_T    _PageDirectoriesSize = ((ULONG_PTR)PDE_TOP                      - (ULONG_PTR)PDE_BASE);
+    SIZE_T    _HyperspaceSize      = ((ULONG_PTR)HYPER_SPACE_END              - (ULONG_PTR)HYPER_SPACE);
+    SIZE_T    _SystemCacheSize     = ((ULONG_PTR)MmSystemCacheEnd             - (ULONG_PTR)MmSystemCacheStart);
+    ULONG_PTR _PagedPoolEnd        = ((ULONG_PTR)MmPagedPoolStart             +            MmSizeOfPagedPoolInBytes);
+    SIZE_T    _SystemPteSize       = ((ULONG_PTR)MmNonPagedPoolExpansionStart - (ULONG_PTR)MmNonPagedSystemStart);
+    SIZE_T    _NPagedPoolExtSize   = ((ULONG_PTR)MmNonPagedPoolEnd            - (ULONG_PTR)MmNonPagedPoolExpansionStart);
+
+#ifdef _M_AMD64
+
+    DPRINT1("MiDbgDumpAddressSpace: FIXME _M_AMD64\n");
+    ASSERT(FALSE);
+
+#else /* _X86_ */
+
+/* Print the memory layout */
+
+DPRINT1("%p - %p (%X) %s\n", KSEG0_BASE,          _BootImageEnd,       MmBootImageSize,             "Boot Loaded Image");
+DPRINT1("%p - %p (%X) %s\n", MmPfnDatabase,       _PfnDatabaseEnd,    _PfnDatabaseSize,             "PFN Database");
+DPRINT1("%p - %p (%X) %s\n", MmNonPagedPoolStart, _NPagedPoolEnd,      MmSizeOfNonPagedPoolInBytes, "ARM3 Non Paged Pool");
+DPRINT1("%p - %p (%X) %s\n", MiSystemViewStart,   _SystemViewEnd,      MmSystemViewSize,            "System View Space");
+DPRINT1("%p - %p (%X) %s\n", MmSessionBase,        MiSessionSpaceEnd, _SessionSize,                 "Session Space");
+DPRINT1("%p - %p (%X) %s\n", PTE_BASE,             PTE_TOP,           _PageTablesSize,              "Page Tables");
+DPRINT1("%p - %p (%X) %s\n", PDE_BASE,             PDE_TOP,           _PageDirectoriesSize,         "Page Directories");
+DPRINT1("%p - %p (%X) %s\n", HYPER_SPACE,          HYPER_SPACE_END,   _HyperspaceSize,              "Hyperspace");
+DPRINT1("%p - %p (%X) %s\n", MmSystemCacheStart,   MmSystemCacheEnd,  _SystemCacheSize,             "System Cache");
+DPRINT1("%p - %p (%X) %s\n", MmPagedPoolStart,    _PagedPoolEnd,       MmSizeOfPagedPoolInBytes,    "ARM3 Paged Pool");
+
+DPRINT1("%p - %p (%X) %s\n", MmNonPagedSystemStart, MmNonPagedPoolExpansionStart, _SystemPteSize,   "System PTE Space");
+DPRINT1("%p - %p (%X) %s\n", MmNonPagedPoolExpansionStart, MmNonPagedPoolEnd, _NPagedPoolExtSize,   "Non Paged Pool Expansion PTE Space");
+
+#endif
+
+}
 
 INIT_FUNCTION
 BOOLEAN
