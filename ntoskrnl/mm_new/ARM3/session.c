b47721e9534c000b29fe3406e686590fbe5f1531
Vadim Galyant <vgal@rambler.ru>
2022-07-22 01:21:50 +0400

    [NTOS:MM_NEW] Add MiInitializeSessionWideAddresses().

--- ntoskrnl/mm_new/ARM3/session.c
+++ ntoskrnl/mm_new/ARM3/session.c
@@ -8,7 +8,28 @@
 
 /* GLOBALS ********************************************************************/
 
+/* Number of initial session IDs */
+#define MI_INITIAL_SESSION_IDS  64
+
+/* Number of session data and tag pages */
+#define MI_SESSION_DATA_PAGES_MAXIMUM (MM_ALLOCATION_GRANULARITY / PAGE_SIZE)
+#define MI_SESSION_TAG_PAGES_MAXIMUM  (MM_ALLOCATION_GRANULARITY / PAGE_SIZE)
+
 PMM_SESSION_SPACE MmSessionSpace;
+KGUARDED_MUTEX MiSessionIdMutex;
+RTL_BITMAP MiSessionWideVaBitMap;
+PRTL_BITMAP MiSessionIdBitmap;
+LIST_ENTRY MiSessionWsList;
+LIST_ENTRY MmWorkingSetExpansionHead;
+PFN_NUMBER MiSessionDataPages;
+PFN_NUMBER MiSessionTagPages;
+PFN_NUMBER MiSessionTagSizePages;
+PFN_NUMBER MiSessionBigPoolPages;
+PFN_NUMBER MiSessionCreateCharge;
+
+extern PVOID MiSessionImageStart;
+extern PVOID MiSessionImageEnd;
+extern PFN_NUMBER MmLowestPhysicalPage;
 
 /* FUNCTIONS ******************************************************************/
 
@@ -103,5 +124,28 @@ MmIsSessionAddress(
     return (MI_IS_SESSION_ADDRESS(Address) ? TRUE : FALSE);
 }
 
+VOID
+NTAPI
+MiInitializeSessionWideAddresses(VOID)
+{
+    PULONG BitMapBuffer;
+    ULONG SizeOfBitMap;
+    ULONG Size;
+
+    DPRINT("MiInitializeSessionWideAddresses()\n");
+
+    SizeOfBitMap = (ULONG)((ULONG_PTR)MiSessionImageEnd - (ULONG_PTR)MiSessionImageStart);
+    SizeOfBitMap /= PAGE_SIZE;
+    Size = (((SizeOfBitMap + 0x1F) / 0x20) * sizeof(ULONG));
+
+    BitMapBuffer = ExAllocatePoolWithTag(PagedPool, Size, '  mM');
+    if (!BitMapBuffer)
+    {
+        KeBugCheckEx(0x7D, MmNumberOfPhysicalPages, MmLowestPhysicalPage, MmHighestPhysicalPage, 0x301);
+    }
+
+    RtlInitializeBitMap(&MiSessionWideVaBitMap, BitMapBuffer, SizeOfBitMap);
+    RtlClearAllBits(&MiSessionWideVaBitMap);
+}
 
 /* EOF */
