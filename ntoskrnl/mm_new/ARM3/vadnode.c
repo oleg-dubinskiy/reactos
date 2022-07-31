7dd1fcc06f91bdc8223211e061f10e7a93f4e093
Vadim Galyant <vgal@rambler.ru>
2022-07-31 20:37:11 +0400

    [NTOS:MM_NEW] Add MiCheckForConflictingVadExistence().

--- ntoskrnl/mm_new/ARM3/vadnode.c
+++ ntoskrnl/mm_new/ARM3/vadnode.c
@@ -4,11 +4,68 @@
 #include <ntoskrnl.h>
 //#define NDEBUG
 #include <debug.h>
+#include "miarm.h"
 
 /* GLOBALS ********************************************************************/
 
 
 /* FUNCTIONS ******************************************************************/
+
+BOOLEAN
+NTAPI
+MiCheckForConflictingVadExistence(
+    _In_ PEPROCESS Process,
+    _In_ ULONG_PTR StartingAddress,
+    _In_ ULONG_PTR EndingAddress)
+{
+    PMMADDRESS_NODE Node;
+    ULONG_PTR StartVpn;
+    ULONG_PTR EndVpn;
+
+    StartVpn = (StartingAddress / PAGE_SIZE);
+    EndVpn = (EndingAddress / PAGE_SIZE);
+
+    DPRINT("MiCheckForConflictingVadExistence: Process %p, Start %p, End %p, (%X:%X)Vpn\n",
+           Process, StartingAddress, EndingAddress, StartVpn, EndVpn);
+
+    /* If the tree is empty, there is no conflict */
+    if (!Process->VadRoot.NumberGenericTableElements)
+        return FALSE;
+
+    /* Start looping from the root node */
+    Node = Process->VadRoot.BalancedRoot.RightChild;
+    if (!Node)
+    {
+        /* If the tree is not empty, this should not be */
+        ASSERT(Node != NULL);
+        return FALSE;
+    }
+
+    while (Node)
+    {
+        DPRINT("MiCheckForConflictingVadExistence: Node %p, StartingVpn %p, EndingVpn %p\n",
+               Node, Node->StartingVpn, Node->EndingVpn);
+
+        /* This address comes after */
+        if (StartVpn > Node->EndingVpn)
+        {
+            Node = Node->RightChild;
+        }
+        else if (EndVpn < Node->StartingVpn)
+        {
+            /* This address ends before the node starts, search on the left */
+            Node = Node->LeftChild;
+        }
+        else
+        {
+            DPRINT("MiCheckForConflictingVadExistence: This address is part of this Node\n");
+            return TRUE;
+        }
+    }
+
+    /* There is no more child */
+    return FALSE;
+}
 
 
 /* EOF */
