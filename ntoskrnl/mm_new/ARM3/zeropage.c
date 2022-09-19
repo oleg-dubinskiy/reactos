e7f5120fa03d441fb6dbe8ed12c4c262dfe1170c
Vadim Galyant <vgal@rambler.ru>
2022-09-19 04:51:17 +0400

    [NTOS:MM_NEW] Add MiFreeInitializationCode().

--- ntoskrnl/mm_new/ARM3/zeropage.c
+++ ntoskrnl/mm_new/ARM3/zeropage.c
@@ -14,6 +14,33 @@ KEVENT MmZeroingPageEvent;
 
 VOID
 NTAPI
+MiFreeInitializationCode(
+    _In_ PVOID InitStart,
+    _In_ PVOID InitEnd)
+{
+    PMMPTE Pte;
+    PFN_NUMBER PagesFreed;
+
+    DPRINT("MiFreeInitializationCode: ... \n");
+
+    /* Get the start PTE */
+    Pte = MiAddressToPte(InitStart);
+
+    if (MI_IS_PHYSICAL_ADDRESS(InitStart))
+    {
+        DPRINT1("MiFreeInitializationCode: FIXME\n");
+        ASSERT(MI_IS_PHYSICAL_ADDRESS(InitStart) == FALSE);
+    }
+
+    /*  Compute the number of pages we expect to free */
+    PagesFreed = (PFN_NUMBER)(MiAddressToPte(InitEnd) - Pte);
+
+    /* Try to actually free them */
+    PagesFreed = MiDeleteSystemPageableVm(Pte, PagesFreed, 0, NULL);
+}
+
+VOID
+NTAPI
 MmZeroPageThread(VOID)
 {
     UNIMPLEMENTED_DBGBREAK();
