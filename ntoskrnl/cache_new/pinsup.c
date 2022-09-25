e8fde5271fcafd8d0b6ac3ca5c8895df85c10c56
Vadim Galyant <vgal@rambler.ru>
2022-09-25 14:53:12 +0400

    [NTOS:CACHE_NEW] Add CcPinFileData().

--- ntoskrnl/cache_new/pinsup.c
+++ ntoskrnl/cache_new/pinsup.c
@@ -147,6 +147,207 @@ CcUnpinFileDataEx(
     }
 }
 
+BOOLEAN
+NTAPI
+CcPinFileData(
+    _In_ PFILE_OBJECT FileObject,
+    _In_ PLARGE_INTEGER FileOffset,
+    _In_ ULONG Length,
+    _In_ BOOLEAN IsNoWrite,
+    _In_ BOOLEAN Flag5,
+    _In_ ULONG PinFlags,
+    _Out_ PCC_BCB* OutBcb,
+    _Out_ PVOID* OutBuffer,
+    _Out_ LARGE_INTEGER* OutBeyondLastByte)
+{
+    KLOCK_QUEUE_HANDLE LockHandle;
+    PSHARED_CACHE_MAP SharedMap;
+    PVACB Vacb = NULL;
+    PCC_BCB Bcb = NULL;
+    LARGE_INTEGER EndFileOffset;
+    LARGE_INTEGER offset;
+    LARGE_INTEGER length;
+    ULONG ActivePage;
+    ULONG ReceivedLength;
+    ULONG MapFlags = 0;
+    BOOLEAN IsLocked;
+    BOOLEAN IsVacbLocked;
+    BOOLEAN Result = FALSE;
+
+    DPRINT("CcPinFileData: %p, [%I64X], %X, %X, %X, %X\n",
+           FileObject, (FileOffset ? FileOffset->QuadPart : 0ll), Length, IsNoWrite, Flag5, PinFlags);
+
+    SharedMap = FileObject->SectionObjectPointer->SharedCacheMap;
+
+    CcGetActiveVacb(SharedMap, &Vacb, &ActivePage, &IsVacbLocked);
+
+    if (Vacb || SharedMap->NeedToZero)
+    {
+        CcFreeActiveVacb(SharedMap, Vacb, ActivePage, IsVacbLocked);
+        Vacb = NULL;
+    }
+
+    ASSERT((FileOffset->QuadPart + (LONGLONG)Length) <= SharedMap->SectionSize.QuadPart);
+
+    *OutBcb = NULL;
+    *OutBuffer = NULL;
+
+    if (PinFlags & PIN_NO_READ)
+    {
+        DPRINT1("CcPinFileData: FIXME\n");
+        ASSERT(FALSE);
+    }
+    else
+    {
+        *OutBuffer = CcGetVirtualAddress(SharedMap, *FileOffset, &Vacb, &ReceivedLength);
+    }
+
+    KeAcquireInStackQueuedSpinLock(&SharedMap->BcbSpinLock, &LockHandle);
+    IsLocked = TRUE;
+
+    //_SEH2_TRY
+
+    EndFileOffset.QuadPart = (FileOffset->QuadPart + Length);
+
+    if (CcFindBcb(SharedMap, FileOffset, &EndFileOffset, &Bcb))
+    {
+        DPRINT1("CcPinFileData: FIXME\n");
+        ASSERT(FALSE);
+    }
+    else
+    {
+        if (PinFlags & PIN_IF_BCB)
+        {
+            Bcb = NULL;
+            Result = FALSE;
+            goto Finish;
+        }
+
+        offset.QuadPart = FileOffset->QuadPart;
+        length.QuadPart = (EndFileOffset.QuadPart - offset.QuadPart);
+
+        length.LowPart += (offset.LowPart & (PAGE_SIZE - 1));
+        ReceivedLength += (offset.LowPart & (PAGE_SIZE - 1));
+
+        if ((!IsNoWrite && !(SharedMap->Flags & SHARE_FL_PIN_ACCESS)) || Flag5)
+        {
+            if (!(offset.LowPart & (PAGE_SIZE - 1)) && Length >= PAGE_SIZE)
+                MapFlags = 3;
+            else
+                MapFlags = 2;
+
+            if (!(length.LowPart & (PAGE_SIZE - 1)))
+                MapFlags |= 4;
+        }
+
+        if (!(SharedMap->Flags & SHARE_FL_MODIFIED_NO_WRITE))
+            IsNoWrite = TRUE;
+
+        *OutBuffer = ((PCHAR)*OutBuffer - (FileOffset->LowPart & (PAGE_SIZE - 1)));
+
+        offset.LowPart &= ~(PAGE_SIZE - 1);
+
+        length.LowPart = ROUND_TO_PAGES(length.LowPart);
+
+        if (length.LowPart > ReceivedLength)
+            length.LowPart = ReceivedLength;
+
+        Bcb = CcAllocateInitializeBcb(SharedMap, Bcb, &offset, &length);
+
+        if (PinFlags & PIN_WAIT)
+        {
+            if (!Bcb)
+            {
+                KeReleaseInStackQueuedSpinLock(&LockHandle);
+                IsLocked = FALSE;
+
+                DPRINT1("CcPinFileData: STATUS_INSUFFICIENT_RESOURCES\n");
+                RtlRaiseStatus(STATUS_INSUFFICIENT_RESOURCES);
+            }
+
+            if (!IsNoWrite)
+            {
+                /* For writing need acquire Bcb */
+                if (PinFlags & PIN_EXCLUSIVE)
+                    ExAcquireResourceExclusiveLite(&Bcb->BcbResource, TRUE);
+                else
+                    ExAcquireSharedStarveExclusive(&Bcb->BcbResource, TRUE);
+            }
+
+            KeReleaseInStackQueuedSpinLock(&LockHandle);
+            IsLocked = FALSE;
+
+            if (PinFlags & PIN_NO_READ)
+            {
+                Result = TRUE;
+                goto Finish;
+            }
+
+            CcMapAndRead(SharedMap, &offset, length.LowPart, MapFlags, TRUE, *OutBuffer);
+
+            KeAcquireInStackQueuedSpinLock(&SharedMap->BcbSpinLock, &LockHandle);
+            if (!Bcb->BaseAddress)
+            {
+                Bcb->BaseAddress = *OutBuffer;
+                Bcb->Vacb = Vacb;
+                Vacb = NULL;
+            }
+            KeReleaseInStackQueuedSpinLock(&LockHandle);
+
+            *OutBuffer = Add2Ptr(Bcb->BaseAddress, (FileOffset->LowPart - Bcb->FileOffset.LowPart));
+
+            Result = TRUE;
+            goto Finish;
+        }
+        else
+        {
+            DPRINT1("CcPinFileData: FIXME\n");
+            ASSERT(FALSE);
+        }
+    }
+
+Finish:
+
+    if ((PinFlags & PIN_NO_READ) && (PinFlags & PIN_EXCLUSIVE) && Bcb && Bcb->BaseAddress)
+    {
+        CcFreeVirtualAddress(Bcb->Vacb);
+
+        Bcb->BaseAddress = NULL;
+        Bcb->Vacb = NULL;
+    }
+
+    //_SEH2_FINALLY
+
+    if (IsLocked)
+        KeReleaseInStackQueuedSpinLock(&LockHandle);
+
+    if (Vacb)
+        CcFreeVirtualAddress(Vacb);
+
+    if (PinFlags & PIN_NO_READ)
+    {
+        DPRINT1("CcPinFileData: FIXME\n");
+        ASSERT(FALSE);
+    }
+
+    if (Result)
+    {
+        *OutBcb = Bcb;
+        OutBeyondLastByte->QuadPart = Bcb->BeyondLastByte.QuadPart;
+    }
+    else
+    {
+        *OutBuffer = NULL;
+
+        if (Bcb)
+            CcUnpinFileDataEx(Bcb, IsNoWrite, 0);
+    }
+
+    return Result;
+
+    //_SEH2_END;
+}
+
 /* PUBLIC FUNCTIONS ***********************************************************/
 
 BOOLEAN
