acc65d7487135e63997569531f1a7b509984fa38
Vadim Galyant <vgal@rambler.ru>
2022-10-16 00:53:57 +0400

    [NTOS:CACHE_NEW] Correcting CcPurgeCacheSection().

--- ntoskrnl/cache_new/fssup.c
+++ ntoskrnl/cache_new/fssup.c
@@ -894,8 +894,13 @@ CcPurgeCacheSection(
             !(SharedMap->Flags & SHARE_FL_WRITE_QUEUED) &&
             !SharedMap->DirtyPages)
         {
-            DPRINT1("CcPurgeCacheSection: FIXME\n");
-            ASSERT(FALSE);
+            RemoveEntryList(&SharedMap->SharedCacheMapLinks);
+            InsertTailList(&CcDirtySharedCacheMapList.SharedCacheMapLinks, &SharedMap->SharedCacheMapLinks);
+
+            LazyWriter.OtherWork = 1;
+
+            if (!LazyWriter.ScanActive)
+                CcScheduleLazyWriteScan(FALSE);
         }
 
         KeReleaseQueuedSpinLock(LockQueueMasterLock, OldIrql);
