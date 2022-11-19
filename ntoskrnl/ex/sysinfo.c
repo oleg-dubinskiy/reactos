ac3343e56b85a0687e64da9dffb72d2fd942165c
Vadim Galyant <vgal@rambler.ru>
2022-11-19 23:14:51 +0300

    [NTOS:EX] Correcting case SystemPerformanceInformation for  NtQuerySystemInformation() - using MmTotalCommittedPages and MmPeakCommitment.

--- ntoskrnl/ex/sysinfo.c
+++ ntoskrnl/ex/sysinfo.c
@@ -723,14 +723,8 @@ QSI_DEF(SystemPerformanceInformation)
     }
 
     Spi->AvailablePages = (ULONG)MmAvailablePages;
-    /*
-     *   Add up all the used "Committed" memory + pagefile.
-     *   Not sure this is right. 8^\
-     */
-    Spi->CommittedPages = MiMemoryConsumers[MC_SYSTEM].PagesUsed +
-                          MiMemoryConsumers[MC_CACHE].PagesUsed +
-                          MiMemoryConsumers[MC_USER].PagesUsed +
-                          MiUsedSwapPages;
+    Spi->CommittedPages = (ULONG)MmTotalCommittedPages;
+
     /*
      *  Add up the full system total + pagefile.
      *  All this make Taskmgr happy but not sure it is the right numbers.
@@ -738,7 +732,7 @@ QSI_DEF(SystemPerformanceInformation)
      */
     Spi->CommitLimit = MmNumberOfPhysicalPages + MiFreeSwapPages + MiUsedSwapPages;
 
-    Spi->PeakCommitment = 0; /* FIXME */
+    Spi->PeakCommitment = MmPeakCommitment;
     Spi->PageFaultCount = 0; /* FIXME */
     Spi->CopyOnWriteCount = 0; /* FIXME */
     Spi->TransitionCount = 0; /* FIXME */
