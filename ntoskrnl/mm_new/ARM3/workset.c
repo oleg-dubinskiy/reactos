dd087b7158aa5e7bf60c37d580f16aca4875bd18
Vadim Galyant <vgal@rambler.ru>
2022-12-06 05:18:52 +0300

    [NTOS:MM_NEW] Implement MmAdjustWorkingSetSize().

--- ntoskrnl/mm_new/ARM3/workset.c
+++ ntoskrnl/mm_new/ARM3/workset.c
@@ -8,6 +8,9 @@
 
 /* GLOBALS ********************************************************************/
 
+SIZE_T MmMinimumWorkingSetSize;
+SIZE_T MmMaximumWorkingSetSize;
+SIZE_T MmPagesAboveWsMinimum;
 
 /* FUNCTIONS ******************************************************************/
 
@@ -146,6 +149,262 @@ MiEmptyWorkingSet(
     return STATUS_SUCCESS;
 }
 
+NTSTATUS
+NTAPI
+MmAdjustWorkingSetSizeEx(
+    SIZE_T WorkingSetMinimumInBytes,
+    SIZE_T WorkingSetMaximumInBytes,
+    BOOLEAN IsSystemCache,
+    BOOLEAN IsIncreaseOkay,
+    ULONG Flags,
+    BOOLEAN* OutIsAddMinSize)
+{
+    PETHREAD Thread = PsGetCurrentThread();
+    PEPROCESS Process;
+    PMMSUPPORT WorkingSet;
+    PMMWSL WorkingSetList;
+    SIZE_T MinWorkingSetSize;
+    SIZE_T MaxWorkingSetSize;
+    SSIZE_T Delta;
+    ULONG TrimAge;
+    BOOLEAN LimitIsGood = TRUE;
+    KIRQL OldIrql;
+    NTSTATUS Status;
+
+    DPRINT("MmAdjustWorkingSetSizeEx: %IX, %IX, %X\n", WorkingSetMinimumInBytes, WorkingSetMaximumInBytes, IsSystemCache);
+
+    *OutIsAddMinSize = FALSE;
+
+    if (IsSystemCache)
+    {
+        Process = NULL;
+        WorkingSet = &MmSystemCacheWs;
+    }
+    else
+    {
+        Process = (PEPROCESS)Thread->Tcb.ApcState.Process;
+        WorkingSet = &Process->Vm;
+    }
+
+    /* Check for special case: empty the working set */
+    if (WorkingSetMinimumInBytes == -1 && WorkingSetMaximumInBytes == -1)
+        return MiEmptyWorkingSet(WorkingSet, TRUE);
+
+    /* Assume success */
+    Status = STATUS_SUCCESS;
+
+    //MmLockPagableSectionByHandle(ExPageLockHandle);
+
+    /* Lock the working set */
+    if (IsSystemCache)
+    {
+        DPRINT1("MmAdjustWorkingSetSizeEx: FIXME\n");
+        ASSERT(FALSE);
+    }
+    else
+    {
+        MiLockProcessWorkingSet(Process, Thread);
+
+        if (Process->VmDeleted)
+        {
+            DPRINT1("MmAdjustWorkingSetSizeEx: STATUS_PROCESS_IS_TERMINATING\n");
+            Status = STATUS_PROCESS_IS_TERMINATING;
+            goto Cleanup;
+        }
+    }
+
+    /* Calculate the actual minimum and maximum working set size to set */
+    if (WorkingSetMinimumInBytes)
+        MinWorkingSetSize = (WorkingSetMinimumInBytes / PAGE_SIZE);
+    else
+        MinWorkingSetSize = WorkingSet->MinimumWorkingSetSize;
+
+    if (WorkingSetMaximumInBytes)
+        MaxWorkingSetSize = (WorkingSetMaximumInBytes / PAGE_SIZE);
+    else
+        MaxWorkingSetSize = WorkingSet->MaximumWorkingSetSize;
+
+    /* Check if the new minimum exceeds the new maximum */
+    if (MinWorkingSetSize > MaxWorkingSetSize)
+    {
+        DPRINT1("MmAdjustWorkingSetSizeEx: STATUS_BAD_WORKING_SET_LIMIT. %IX, %IX\n", MinWorkingSetSize, MaxWorkingSetSize);
+        Status = STATUS_BAD_WORKING_SET_LIMIT;
+        goto Cleanup;
+    }
+
+    /* Check if the new maximum exceeds the global maximum */
+    if (MaxWorkingSetSize > MmMaximumWorkingSetSize)
+    {
+        DPRINT1("MmAdjustWorkingSetSizeEx: STATUS_WORKING_SET_LIMIT_RANGE\n");
+        MaxWorkingSetSize = MmMaximumWorkingSetSize;
+        Status = STATUS_WORKING_SET_LIMIT_RANGE;
+    }
+
+    /* Check if the new minimum is below the global minimum */
+    if (MinWorkingSetSize < MmMinimumWorkingSetSize)
+    {
+        DPRINT1("MmAdjustWorkingSetSizeEx: STATUS_WORKING_SET_LIMIT_RANGE\n");
+        MinWorkingSetSize = MmMinimumWorkingSetSize;
+        Status = STATUS_WORKING_SET_LIMIT_RANGE;
+    }
+
+    /* Check if the new minimum exceeds the new maximum */
+    if (MinWorkingSetSize > MmMaximumWorkingSetSize)
+    {
+        DPRINT1("MmAdjustWorkingSetSizeEx: STATUS_WORKING_SET_LIMIT_RANGE\n");
+        MinWorkingSetSize = MmMaximumWorkingSetSize;
+        Status = STATUS_WORKING_SET_LIMIT_RANGE;
+    }
+
+    WorkingSetList = WorkingSet->VmWorkingSetList;
+
+    if (MaxWorkingSetSize <= (WorkingSetList->FirstDynamic + 8))
+    {
+        DPRINT1("MmAdjustWorkingSetSizeEx: STATUS_BAD_WORKING_SET_LIMIT\n");
+        Status = STATUS_BAD_WORKING_SET_LIMIT;
+        goto Cleanup;
+    }
+
+    if ((Flags & 4) ||
+        (WorkingSet->Flags.MinimumWorkingSetHard && !(Flags & 8)) ||
+        (Flags & 1) ||
+        (WorkingSet->Flags.MaximumWorkingSetHard && !(Flags & 2)))
+    {
+        if (MaxWorkingSetSize <= (MinWorkingSetSize + 8) ||
+            (IsSystemCache && MaxWorkingSetSize < 0x1000))
+        {
+            DPRINT1("MmAdjustWorkingSetSizeEx: STATUS_BAD_WORKING_SET_LIMIT\n");
+            Status = STATUS_BAD_WORKING_SET_LIMIT;
+            goto Cleanup;
+        }
+    }
+
+    /* Calculate the minimum WS size adjustment and check if we increase */
+    Delta = (MinWorkingSetSize - WorkingSet->MinimumWorkingSetSize);
+
+    OldIrql = MiLockPfnDb(APC_LEVEL);
+
+    if (Delta > 0)
+    {
+        *OutIsAddMinSize = TRUE;
+
+        /* Is increasing ok? */
+        if (!IsIncreaseOkay)
+        {
+            DPRINT1("MmAdjustWorkingSetSizeEx: Privilege for WS size increase not held\n");
+            MiUnlockPfnDb(OldIrql, APC_LEVEL);
+            Status = STATUS_PRIVILEGE_NOT_HELD;
+            goto Cleanup;
+        }
+
+        /* Check if the number of available pages is large enough */
+        if ((SPFN_NUMBER)((Delta / (PAGE_SIZE / sizeof(ULONG)))) > (SPFN_NUMBER)(MmAvailablePages - 0x80))
+        {
+            DPRINT1("MmAdjustWorkingSetSizeEx: Not enough available pages\n");
+            MiUnlockPfnDb(OldIrql, APC_LEVEL);
+            Status = STATUS_INSUFFICIENT_RESOURCES;
+            goto Cleanup;
+        }
+
+        /* Check if there are enough resident available pages */
+        if (Delta > (MmResidentAvailablePages - MmSystemLockPagesCount - 0x100))
+        {
+            DPRINT1("MmAdjustWorkingSetSizeEx: Not enough resident pages\n");
+            MiUnlockPfnDb(OldIrql, APC_LEVEL);
+            Status = STATUS_INSUFFICIENT_RESOURCES;
+            goto Cleanup;
+        }
+    }
+
+    /* Update resident available pages */
+    if (Delta)
+        InterlockedExchangeAddSizeT(&MmResidentAvailablePages, -Delta);
+
+    MiUnlockPfnDb(OldIrql, APC_LEVEL);
+
+    if (MaxWorkingSetSize < WorkingSetList->LastInitializedWsle &&
+        MaxWorkingSetSize < WorkingSet->WorkingSetSize)
+    {
+        if (WorkingSetList->FirstDynamic + 8 >= MaxWorkingSetSize)
+        {
+            DPRINT1("MmAdjustWorkingSetSizeEx: STATUS_BAD_WORKING_SET_LIMIT\n");
+            InterlockedExchangeAddSizeT(&MmResidentAvailablePages, Delta);
+            Status = STATUS_BAD_WORKING_SET_LIMIT;
+            goto Cleanup;
+        }
+
+        for (TrimAge = 2; ; TrimAge--)
+        {
+            ASSERT(TrimAge <= 2); // MI_PASS0_TRIM_AGE
+
+            DPRINT1("MmAdjustWorkingSetSizeEx: FIXME MiTrimWorkingSet()\n");
+            ASSERT(FALSE);
+            //MiTrimWorkingSet((WorkingSet->WorkingSetSize - MaxWorkingSetSize), WorkingSet, TrimAge);
+
+            if (WorkingSet->WorkingSetSize <= MaxWorkingSetSize)
+                break;
+
+            if (!TrimAge)
+            {
+                DPRINT1("MmAdjustWorkingSetSizeEx: STATUS_BAD_WORKING_SET_LIMIT\n");
+                LimitIsGood = FALSE;
+                Status = STATUS_BAD_WORKING_SET_LIMIT;
+                break;
+            }
+        }
+    }
+
+    if ((LONG)WorkingSet->WorkingSetSize > (LONG)WorkingSet->MinimumWorkingSetSize)
+        InterlockedExchangeAddSizeT(&MmPagesAboveWsMinimum, -((LONG)WorkingSet->WorkingSetSize - (LONG)WorkingSet->MinimumWorkingSetSize));
+
+    if ((LONG)WorkingSet->WorkingSetSize > (LONG)MinWorkingSetSize)
+        InterlockedExchangeAddSizeT(&MmPagesAboveWsMinimum, ((LONG)WorkingSet->WorkingSetSize - (LONG)MinWorkingSetSize));
+
+    if (LimitIsGood)
+    {
+        WorkingSet->MaximumWorkingSetSize = MaxWorkingSetSize;
+        WorkingSet->MinimumWorkingSetSize = MinWorkingSetSize;
+
+        if (Flags)
+        {
+            OldIrql = MiAcquireExpansionLock();
+
+            if (Flags & 4)
+                WorkingSet->Flags.MinimumWorkingSetHard = 1;
+            else if (Flags & 8)
+                WorkingSet->Flags.MinimumWorkingSetHard = 0;
+
+            if (Flags & 1)
+                WorkingSet->Flags.MaximumWorkingSetHard = 1;
+            else if (Flags & 2)
+                WorkingSet->Flags.MaximumWorkingSetHard = 0;
+
+            MiReleaseExpansionLock(OldIrql);
+        }
+    }
+    else
+    {
+        InterlockedExchangeAddSizeT(&MmResidentAvailablePages, Delta);
+    }
+
+    ASSERT((WorkingSetList->FirstFree <= WorkingSetList->LastInitializedWsle) || (WorkingSetList->FirstFree == 0xFFFFFFF)); // WSLE_NULL_INDEX
+
+Cleanup:
+
+    if (IsSystemCache)
+    {
+        DPRINT1("MmAdjustWorkingSetSizeEx: FIXME\n");
+        ASSERT(FALSE);
+    }
+    else
+    {
+        MiUnlockProcessWorkingSet(Process, Thread);
+    }
+
+    //MmUnlockPagableImageSection(ExPageLockHandle);
+    return Status;
+}
+
 /* PUBLIC FUNCTIONS ***********************************************************/
 
 NTSTATUS
@@ -156,8 +415,14 @@ MmAdjustWorkingSetSize(
     _In_ ULONG SystemCache,
     _In_ BOOLEAN IncreaseOkay)
 {
-    UNIMPLEMENTED_DBGBREAK();
-    return STATUS_NOT_IMPLEMENTED;
+    BOOLEAN dummy;
+
+    return MmAdjustWorkingSetSizeEx(WorkingSetMinimumInBytes,
+                                    WorkingSetMaximumInBytes,
+                                    SystemCache,
+                                    IncreaseOkay,
+                                    0,
+                                    &dummy);
 }
 
 /* EOF */
