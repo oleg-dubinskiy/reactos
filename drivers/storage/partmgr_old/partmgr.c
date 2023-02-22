0b9e8d1992a25c026081d0cefbe8d2a6d3d63d97
Vadim Galyant <vgal@rambler.ru>
2023-02-22 11:45:49 +0300

    [PARTMGR_OLD] Add the partition manager driver "Partition Manager" (partmgr.sys).

---
+++ drivers/storage/partmgr_old/partmgr.c
@@ -0,0 +1,19 @@
+/*
+ * PROJECT:     Partition manager driver
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Main file
+ * COPYRIGHT:   Copyright 2018, 2019, 2023 Vadim Galyant <vgal@rambler.ru>
+ */
+
+#include "partmgr.h"
+
+//#define NDEBUG
+#include <debug.h>
+
+/* GLOBALS *******************************************************************/
+
+
+/* FUNCTIONS ****************************************************************/
+
+
+/* EOF */
---
+++ drivers/storage/partmgr_old/partmgr.h
@@ -0,0 +1,16 @@
+/*
+ * PROJECT:     Partition manager driver
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Main header file
+ * COPYRIGHT:   Copyright 2018, 2019, 2023 Vadim Galyant <vgal@rambler.ru>
+ */
+
+#ifndef _PARTMGR_H_
+#define _PARTMGR_H_
+
+#include <ntifs.h>
+
+
+#endif /* _PARTMGR_H_ */
+
+/* EOF */
