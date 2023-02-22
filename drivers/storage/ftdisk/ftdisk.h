6b8111d31b3a189c40b7f64bafe36847d8a02ab5
Vadim Galyant <vgal@rambler.ru>
2023-02-22 11:40:44 +0300

    [FTDISK] Add the volume manager driver "FT Disk Driver" (ftdisk.sys).

---
+++ drivers/storage/ftdisk/ftdisk.h
@@ -0,0 +1,15 @@
+/*
+ * PROJECT:     Volume manager driver
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Main header file
+ * COPYRIGHT:   Copyright 2018, 2019, 2023 Vadim Galyant <vgal@rambler.ru>
+ */
+
+#ifndef _FTDISK_H_
+#define _FTDISK_H_
+
+#include <ntifs.h>
+
+#endif /* _FTDISK_H_ */
+
+/* EOF */
