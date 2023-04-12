c86f21e62b5556934c36049ab12e5d29cdd3e5ed
Vadim Galyant <vgal@rambler.ru>
2023-04-12 10:59:54 +0400

    [ACPI_OLD] Add "dispatch.c" and "registry.c" files.

---
+++ drivers/bus/acpi_old/registry.c
@@ -0,0 +1,19 @@
+/*
+ * PROJECT:     ACPI driver for NT 5.x
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     Working with the registry
+ * COPYRIGHT:   Copyright 2019, 2023 Vadim Galyant <vgal@rambler.ru>
+ */
+
+#include "acpi.h"
+
+//#define NDEBUG
+#include <debug.h>
+
+/* GLOBALS *******************************************************************/
+
+
+/* FUNCTIONS *****************************************************************/
+
+
+/* EOF */
