3c482c777e08846a6c4058454439d36a0c47ff15
Vadim Galyant <vgal@rambler.ru>
2023-04-14 02:44:09 +0400

    [ACPI_OLD] Add ACPIInitializeAMLI().

--- drivers/bus/acpi_old/amli.c
+++ drivers/bus/acpi_old/amli.c
@@ -12,8 +12,73 @@
 
 /* GLOBALS *******************************************************************/
 
+ULONG AMLIMaxCTObjs;
 
 /* FUNCTIONS ****************************************************************/
 
+NTSTATUS
+NTAPI
+OSInitializeCallbacks(VOID)
+{
+    UNIMPLEMENTED_DBGBREAK();
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+__cdecl
+AMLIInitialize(
+    _In_ ULONG CtxtBlkSize,
+    _In_ ULONG GlobalHeapBlkSize,
+    _In_ ULONG InitFlags,
+    _In_ ULONG TimeSliceLen,
+    _In_ ULONG TimeSliceInterval,
+    _In_ ULONG MaxContextsDepth)
+{
+    UNIMPLEMENTED_DBGBREAK();
+    return STATUS_NOT_IMPLEMENTED;
+}
+
+NTSTATUS
+NTAPI
+ACPIInitializeAMLI(VOID)
+{
+    ULONG CtxtBlkSize;
+    ULONG GlobalHeapBlkSize;
+    ULONG InitFlags;
+    ULONG TimeSliceLength;
+    ULONG TimeSliceInterval;
+    ULONG Length;
+
+    PAGED_CODE();
+    DPRINT("ACPIInitializeAMLI()\n");
+
+    Length = 4;
+    if (OSReadRegValue("AMLIInitFlags", 0, &InitFlags, &Length) < 0)
+        InitFlags = 0;
+
+    Length = 4;
+    if (OSReadRegValue("AMLICtxtBlkSize", 0, &CtxtBlkSize, &Length) < 0)
+        CtxtBlkSize = 0;
+
+    Length = 4;
+    if (OSReadRegValue("AMLIGlobalHeapBlkSize", 0, &GlobalHeapBlkSize, &Length) < 0)
+        GlobalHeapBlkSize = 0;
+
+    Length = 4;
+    if (OSReadRegValue("AMLITimeSliceLength", 0, &TimeSliceLength, &Length) < 0)
+        TimeSliceLength = 0;
+
+    Length = 4;
+    if (OSReadRegValue("AMLITimeSliceInterval", 0, &TimeSliceInterval, &Length) < 0)
+        TimeSliceInterval = 0;
+
+    Length = 4;
+    if (OSReadRegValue("AMLIMaxCTObjs", 0, &AMLIMaxCTObjs, &Length) < 0)
+        AMLIMaxCTObjs = 0;
+
+    OSInitializeCallbacks();
+
+    return AMLIInitialize(CtxtBlkSize, GlobalHeapBlkSize, InitFlags, TimeSliceLength, TimeSliceInterval, AMLIMaxCTObjs);
+}
 
 /* EOF */
--- drivers/bus/acpi_old/amli.h
+++ drivers/bus/acpi_old/amli.h
@@ -10,5 +10,11 @@
 
 /* FUNCTIONS ****************************************************************/
 
+NTSTATUS
+NTAPI
+ACPIInitializeAMLI(
+    VOID
+);
+
 
 /* EOF */
