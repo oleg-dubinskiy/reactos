02097b2226965b4945e94489a5114532c9e69c45
Vadim Galyant <vgal@rambler.ru>
2023-04-13 22:25:37 +0400

    [ACPI_OLD] Add "amli.c" and "amli.h" files.

---
+++ drivers/bus/acpi_old/amli.h
@@ -0,0 +1,14 @@
+/*
+ * PROJECT:     ACPI driver for NT 5.x
+ * LICENSE:     GPL-2.0-or-later (https://spdx.org/licenses/GPL-2.0-or-later)
+ * PURPOSE:     ACPI Machine Language Interpreter (AMLI) header
+ * COPYRIGHT:   Copyright 2019, 2023 Vadim Galyant <vgal@rambler.ru>
+ */
+
+/* STRUCTURES ***************************************************************/
+
+
+/* FUNCTIONS ****************************************************************/
+
+
+/* EOF */
